a4d705bd207ecbfd7f10c3e4539fc6864c28aefb
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2024-03-29 14:13:35 +0100

    WIP: Branch rebased from main

--- src/diffCheck.hh
+++ src/diffCheck.hh
@@ -1,11 +1,12 @@
 #pragma once
 
 
+// include CGAL 
+
 #include <open3d/Open3D.h>
 
 // diffCheck includes
 #include "diffCheck/geometry/DFPointCloud.hh"
 #include "diffCheck/geometry/DFMesh.hh"
 #include "diffCheck/IOManager.hh"
-#include "diffCheck/registration/Registration.hh"
 #include "diffCheck/visualizer.hh"
