b24eed3090ed8dbd01e7ecdf48266dfadd0bd09f
Andrea Settimi <andreasettimi39@gmail.com>
2024-03-30 14:04:00 +0100

    FIX: minor const for logger

--- src/diffCheck.hh
+++ src/diffCheck.hh
@@ -5,7 +5,7 @@
 
 // diffCheck includes
 #include "diffCheck/log.hh"
-diffCheck::Log LOG = diffCheck::Log();
+const diffCheck::Log LOG = diffCheck::Log();
 
 #include "diffCheck/geometry/DFPointCloud.hh"
 #include "diffCheck/geometry/DFMesh.hh"
