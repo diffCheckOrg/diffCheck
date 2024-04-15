fcffcb4da850b1359a28f5cbf7cb81ff647a17b4
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2024-04-15 23:34:02 +0200

    WIP-FIX: improvements on documentation and missing doxygen tags

--- src/diffCheck/registration/globalregistration.hh
+++ src/diffCheck/registration/globalregistration.hh
@@ -11,7 +11,8 @@ class GlobalRegistration
     public:
 
     /** 
-     * @brief Compute the "point to point" distance between two point clouds. 
+    * @brief Compute the "point to point" distance between two point clouds. 
+    * 
     * For every point in the source point cloud it looks in the KDTree of the target point cloud and finds the closest point.
     * It returns a vector of distances, one for each point in the source point cloud.
     * @param source The source diffCheck point cloud
@@ -29,7 +30,8 @@ class GlobalRegistration
     * Very simply, point features are values computed on a point cloud (for example the normal of a point, the curvature, etc.).
     * point features historigrams generalize this concept by computing point features in a local neighborhood of a point, stored as higher-dimentional historigrams.
     * 
-    * Quite important for us: the resultant hyperspace is dependent on the quality of the surface normal estimations at each point (if pc noisy, historigram different).
+    * @note The FPFH hyperspace is dependent on the quality of the surface normal estimations at each point (if pc noisy, historigram different).
+    * 
     * @param source the source diffCheck point cloud
     * @param target the target diffCheck point cloud
     * @param voxelSize the size of the voxels used to downsample the point clouds. A higher value will result in a more coarse point cloud (less resulting points).
@@ -53,16 +55,21 @@ class GlobalRegistration
 
     /**
     * @brief Fast Global Registration based on Correspondence using (Fast) Point Feature Histograms (FPFH) on the source and target point clouds
-    * Little information on this registration method compared to the previous one.
-    * If understood correctly, this method finds keypoints in the FPFH hyperspaces of the source and target point clouds and then tries to match them, instead of using all the features.
-    * https://pcl.readthedocs.io/projects/tutorials/en/latest/correspondence_grouping.html 
+    * 
+    * Little information on this registration method compared to Fast Global Registration Feature Matching.
+    * If understood correctly, this method finds keypoints in the FPFH hyperspaces of the source and target point clouds and then tries to match them, instead of using all the feature historigrams.
+    * 
+    * @note The FPFH hyperspace is dependent on the quality of the surface normal estimations at each point (if pc noisy, historigram different).
+    * 
     * @param source the source diffCheck point cloud
     * @param target the target diffCheck point cloud
     * @param voxelSize the size of the voxels used to downsample the point clouds. A higher value will result in a more coarse point cloud (less resulting points).
     * @param radiusKDTreeSearch the radius used to search for neighbors in the KDTree. It is used for the calculation of FPFHFeatures
     * @param maxNeighborKDTreeSearch the maximum number of neighbors to search for in the KDTree. It is used for the calculation of FPFHFeatures
     * @param iterationNumber the number of iterations to run the RanSaC registration algorithm
-    * @param maxTupleCount the maximum number of tuples to consider in the FPFH hyperspace
+    * @param maxTupleCount the maximum number of tuples to consider in the FPFH hyperspace 
+    * 
+    * @see https://pcl.readthedocs.io/projects/tutorials/en/latest/correspondence_grouping.html (from PCL, not Open3D)
     */ 
     static open3d::pipelines::registration::RegistrationResult O3DFastGlobalRegistrationBasedOnCorrespondence(std::shared_ptr<geometry::DFPointCloud> source, 
                                                                                                               std::shared_ptr<geometry::DFPointCloud> target,
@@ -74,12 +81,12 @@ class GlobalRegistration
                                                                                                               int maxTupleCount = 500);
     /**
     * @brief Ransac registration based on Feature Matching using (Fast) Point Feature Histograms (FPFH) on the source and target point clouds
-    * Correspondances are computed between the source and target point clouds.
+    * Correspondances are computed between the source and target point clouds FPFH hyperspaces.
     * Then, a transformation is computed that minimizes the error between the correspondances. 
     * If the error is above a certain threshold, the transformation is discarded and a new one is computed.
-
-    * In practice, Open3D gives little information about the feature correspondence, compared to the FGR methods
-
+    * 
+    * @note The FPFH hyperspace is dependent on the quality of the surface normal estimations at each point (if pc noisy, historigram different).
+    * 
     * @param source the source diffCheck point cloud
     * @param target the target diffCheck point cloud
     * @param voxelSize the size of the voxels used to downsample the point clouds. A higher value will result in a more coarse point cloud (less resulting points).
@@ -97,13 +104,17 @@ class GlobalRegistration
                                                                                          int correspondenceSetSize = 200);
     /**
     * @brief Ransac registration based on Feature Matching using (Fast) Point Feature Histograms (FPFH) on the source and target point clouds
-    * https://www.open3d.org/docs/release/tutorial/pipelines/global_registration.html#RANSAC
+    * 
+    * If understood correctly, this method finds keypoints in the FPFH hyperspaces of the source and target point clouds and then tries to match them, instead of using all the feature historigrams.
+    * 
     * @param source the source diffCheck point cloud
     * @param target the target diffCheck point cloud
     * @param voxelSize the size of the voxels used to downsample the point clouds. A higher value will result in a more coarse point cloud (less resulting points).
     * @param radiusKDTreeSearch the radius used to search for neighbors in the KDTree. It is used for the calculation of FPFHFeatures
     * @param maxNeighborKDTreeSearch the maximum number of neighbors to search for in the KDTree. It is used for the calculation of FPFHFeatures
     * @param maxCorrespondenceDistance the maximum distance between correspondences.
+    * 
+    * @see https://www.open3d.org/docs/release/tutorial/pipelines/global_registration.html#RANSAC (from PCL, not Open3D)
     */
     static open3d::pipelines::registration::RegistrationResult O3DRansacOnFeatureMatching(std::shared_ptr<geometry::DFPointCloud> source, 
                                                                                           std::shared_ptr<geometry::DFPointCloud> target,
