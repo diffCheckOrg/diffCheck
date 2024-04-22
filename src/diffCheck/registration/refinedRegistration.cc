e5dcdf51dc81103e51610eb2e00f37ddf373fa77
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2024-04-22 14:49:29 +0200

    WIP: refined registrations implemented

--- src/diffCheck/registration/refinedRegistration.cc
+++ src/diffCheck/registration/refinedRegistration.cc
@@ -9,6 +9,17 @@ namespace diffCheck::registration
     {
         std::shared_ptr<open3d::geometry::PointCloud> O3Dsource = source->Cvt2O3DPointCloud();
         std::shared_ptr<open3d::geometry::PointCloud> O3Dtarget = target->Cvt2O3DPointCloud();
+        open3d::pipelines::registration::RegistrationResult result = open3d::pipelines::registration::RegistrationICP(*O3Dsource, 
+                                                                                                                      *O3Dtarget, 
+                                                                                                                      maxCorrespondenceDistance);
+        return result;
+    }
+    open3d::pipelines::registration::RegistrationResult RefinedRegistration::O3DGeneralizedICP(std::shared_ptr<geometry::DFPointCloud> source, 
+                                                                                  std::shared_ptr<geometry::DFPointCloud> target,
+                                                                                  double maxCorrespondenceDistance)
+    {
+        std::shared_ptr<open3d::geometry::PointCloud> O3Dsource = source->Cvt2O3DPointCloud();
+        std::shared_ptr<open3d::geometry::PointCloud> O3Dtarget = target->Cvt2O3DPointCloud();
         open3d::pipelines::registration::RegistrationResult result = open3d::pipelines::registration::RegistrationICP(*O3Dsource, *O3Dtarget, maxCorrespondenceDistance);
         return result;
     }
