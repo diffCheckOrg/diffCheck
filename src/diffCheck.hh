bd6de0a33a7493175fe386d471e760301600404d
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2024-05-02 17:46:15 +0200

    ADD: ICP refined registrations with exposed parameters

--- src/diffCheck.hh
+++ src/diffCheck.hh
@@ -12,4 +12,4 @@ const diffCheck::Log LOG = diffCheck::Log();
 #include "diffCheck/geometry/DFMesh.hh"
 #include "diffCheck/IOManager.hh"
 #include "diffCheck/visualizer.hh"
-#include "diffCheck/registration/refinedRegistration.hh"
+#include "diffCheck/registrations/DFRefinedRegistration.hh"
--- src/diffCheckApp.cc
+++ src/diffCheckApp.cc
@@ -15,13 +15,13 @@ int main()
   std::shared_ptr<diffCheck::geometry::DFPointCloud> dfPointCloudPtrGroundTruth = std::make_shared<diffCheck::geometry::DFPointCloud>();
   std::shared_ptr<diffCheck::geometry::DFPointCloud> dfPointCloudPtrGroundTruthFromMesh = std::make_shared<diffCheck::geometry::DFPointCloud>();
   std::shared_ptr<diffCheck::geometry::DFMesh> dfMeshPtr = std::make_shared<diffCheck::geometry::DFMesh>();
+  std::shared_ptr<diffCheck::geometry::DFPointCloud> dfGroundTruth = std::make_shared<diffCheck::geometry::DFPointCloud>();
 
   // std::string pathCloud = R"(C:\Users\andre\Downloads\scan_data_normals.ply\scan_data_normals.ply)";
   // std::string pathMesh = R"(F:\diffCheck\assets\dataset\mesh_fromRh_unfixedLength.ply)";
   // std::string pathMesh = R"(F:\diffCheck\temp\03_mesh.ply)";
 
   // create a sphere from o3d
-  auto mesh = open3d::geometry::TriangleMesh::CreateSphere(1.0, 4);
 
   std::string pathCloud = R"(C:\Users\localuser\Downloads\00_pt.ply)";
   std::string pathMesh = R"(C:\Users\localuser\Downloads\00_mesh.ply)";
@@ -30,24 +30,30 @@ int main()
   dfMeshPtr->LoadFromPLY(pathMesh);
   dfPointCloudPtr->LoadFromPLY(pathCloud);
 
-  std::shared_ptr<diffCheck::geometry::DFPointCloud> dfGroundTruth;
   dfGroundTruth->Cvt2DFPointCloud(dfMeshPtr->Cvt2O3DTriangleMesh()->SamplePointsUniformly(10000));
+  Eigen::Matrix4d transformation = Eigen::Matrix4d::Identity();
+  transformation(0, 3) = 0.0;
+  transformation(1, 3) = -0.02;
+  transformation(2, 3) = 0.02;
+  Eigen::Matrix3d rotation;
+  rotation = Eigen::AngleAxisd(0, Eigen::Vector3d::UnitY());
+  transformation.block<3, 3>(0, 0) = rotation * transformation.block<3, 3>(0, 0);
 
-  // DIFFCHECK_INFO("test");
-  // DIFFCHECK_WARN("test");
-  // DIFFCHECK_ERROR("test");
-  // DIFFCHECK_FATAL("test");
+  dfPointCloudPtr->ApplyTransformation(transformation);
 
+  diffCheck::transformation::DFTransformation localRegistration = diffCheck::registration::RefinedRegistration::O3DICP(dfPointCloudPtr, dfGroundTruth, 0.05);
 
-  dfMeshPtr->Cvt2DFMesh(mesh);
+  diffCheck::transformation::DFTransformation otherlocalRegistration = diffCheck::registration::RefinedRegistration::O3DGeneralizedICP(dfPointCloudPtr, dfGroundTruth, 0.05);
+  dfPointCloudPtr->ApplyTransformation(localRegistration);
 
-  // dfPointCloudPtr->LoadFromPLY(pathCloud);
-
+  std::cout<<localRegistration.TransformationMatrix<<std::endl;
+  std::cout<<otherlocalRegistration.TransformationMatrix<<std::endl;
+  
   std::shared_ptr<diffCheck::visualizer::Visualizer> vis = std::make_shared<diffCheck::visualizer::Visualizer>();
   // vis->AddPointCloud(dfPointCloudPtr);
   vis->AddMesh(dfMeshPtr);
+  vis->AddPointCloud(dfGroundTruth);
+  vis->AddPointCloud(dfPointCloudPtr);
   vis->Run();
-
-
   return 0;
 }
