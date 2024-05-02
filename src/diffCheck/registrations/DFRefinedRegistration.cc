a3b23a298f432e9da895f9c93cc6b789ff522c75
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2024-05-02 17:45:14 +0200

    ADD: ICP refined registrations with exposed parameters

--- src/diffCheck/registrations/DFRefinedRegistration.cc
+++ src/diffCheck/registrations/DFRefinedRegistration.cc
@@ -1,32 +1,101 @@
-#include "refinedRegistration.hh"
+#include "DFRefinedRegistration.hh"
 
 
 namespace diffCheck::registration
 {
-    open3d::pipelines::registration::RegistrationResult RefinedRegistration::O3DICP(
+    diffCheck::transformation::DFTransformation RefinedRegistration::O3DICP(
         std::shared_ptr<geometry::DFPointCloud> source, 
         std::shared_ptr<geometry::DFPointCloud> target,
-        double maxCorrespondenceDistance)
+        double maxCorrespondenceDistance,
+        bool scalingForPointToPointTransformationEstimation,
+        double relativeFitness,
+        double relativeRMSE,
+        int maxIteration,
+        bool usePointToPlane)
     {
+        std::vector<Eigen::Vector3d> minMax = source->ComputeBoundingBox();
+        double scale = (minMax[1] - minMax[0]).norm();
+        double absoluteMaxCorrespondenceDistance = maxCorrespondenceDistance * scale;
+
+        std::cout<<"Absolute Max Correspondence Distance: "<<absoluteMaxCorrespondenceDistance<<std::endl;
         std::shared_ptr<open3d::geometry::PointCloud> O3Dsource = source->Cvt2O3DPointCloud();
         std::shared_ptr<open3d::geometry::PointCloud> O3Dtarget = target->Cvt2O3DPointCloud();
-        open3d::pipelines::registration::RegistrationResult result = open3d::pipelines::registration::RegistrationICP(
-            *O3Dsource, 
-            *O3Dtarget, 
-            maxCorrespondenceDistance);
-        return result;
+        Eigen::Matrix4d initialTransformation = Eigen::Matrix4d::Identity();
+        open3d::pipelines::registration::ICPConvergenceCriteria criteria
+            = open3d::pipelines::registration::ICPConvergenceCriteria(
+                relativeFitness, 
+                relativeRMSE,
+                maxIteration);
+
+        open3d::pipelines::registration::RegistrationResult result;
+
+        if(usePointToPlane)
+        {
+            open3d::pipelines::registration::TransformationEstimationPointToPlane transformation_estimation 
+                = open3d::pipelines::registration::TransformationEstimationPointToPlane();
+            result = open3d::pipelines::registration::RegistrationICP(
+                *O3Dsource, 
+                *O3Dtarget, 
+                absoluteMaxCorrespondenceDistance,
+                initialTransformation,
+                transformation_estimation,
+                criteria);
+        }
+        else
+        {
+            open3d::pipelines::registration::TransformationEstimationPointToPoint transformation_estimation 
+                = open3d::pipelines::registration::TransformationEstimationPointToPoint(scalingForPointToPointTransformationEstimation);
+            result = open3d::pipelines::registration::RegistrationICP(
+                *O3Dsource, 
+                *O3Dtarget, 
+                absoluteMaxCorrespondenceDistance,
+                initialTransformation,
+                transformation_estimation,
+                criteria);
+        }
+        diffCheck::transformation::DFTransformation transformation 
+            = diffCheck::transformation::DFTransformation(result.transformation_);
+        return transformation;
     }
-    open3d::pipelines::registration::RegistrationResult RefinedRegistration::O3DGeneralizedICP(
-        std::shared_ptr<geometry::DFPointCloud> source, 
+    diffCheck::transformation::DFTransformation RefinedRegistration::O3DGeneralizedICP(
+        std::shared_ptr<geometry::DFPointCloud> source,
         std::shared_ptr<geometry::DFPointCloud> target,
-        double maxCorrespondenceDistance)
+         double maxCorrespondenceDistance,
+            int maxIteration,
+            double relativeFitness,
+            double relativeRMSE)
     {
+        std::vector<Eigen::Vector3d> minMax = source->ComputeBoundingBox();
+        double scale = (minMax[1] - minMax[0]).norm();
+        double absoluteMaxCorrespondenceDistance = maxCorrespondenceDistance * scale;
+
         std::shared_ptr<open3d::geometry::PointCloud> O3Dsource = source->Cvt2O3DPointCloud();
         std::shared_ptr<open3d::geometry::PointCloud> O3Dtarget = target->Cvt2O3DPointCloud();
-        open3d::pipelines::registration::RegistrationResult result = open3d::pipelines::registration::RegistrationICP(
-            *O3Dsource, 
-            *O3Dtarget, 
-            maxCorrespondenceDistance);
-        return result;
+
+        O3Dsource->EstimateCovariances();
+        O3Dtarget->EstimateCovariances();
+
+        Eigen::Matrix4d initialTransformation = Eigen::Matrix4d::Identity();
+        open3d::pipelines::registration::ICPConvergenceCriteria criteria
+            = open3d::pipelines::registration::ICPConvergenceCriteria(
+                relativeFitness, 
+                relativeRMSE,
+                maxIteration);
+
+        open3d::pipelines::registration::TransformationEstimationForGeneralizedICP transformation_estimation 
+            = open3d::pipelines::registration::TransformationEstimationForGeneralizedICP();
+
+        open3d::pipelines::registration::RegistrationResult result 
+            = open3d::pipelines::registration::RegistrationICP(
+                *O3Dsource, 
+                *O3Dtarget, 
+                absoluteMaxCorrespondenceDistance,
+                initialTransformation,
+                transformation_estimation,
+                criteria);
+
+        diffCheck::transformation::DFTransformation transformation 
+            = diffCheck::transformation::DFTransformation(result.transformation_);
+        return transformation;
     }
 }
--- src/diffCheck/registrations/DFRefinedRegistration.hh
+++ src/diffCheck/registrations/DFRefinedRegistration.hh
@@ -14,12 +14,22 @@ namespace diffCheck::registration
          * @param source DFPointCloud source point cloud
          * @param target DFPointCloud Target point cloud
          * @param maxCorrespondenceDistance Maximum relative correspondence distance. 0.01 means 1% of the bounding box diagonal
+         * @param scalingForPointToPointTransformationEstimation Enable scaling for point-to-point transformation estimation. by default it is false, to only allow rigid transformations
+         * @param maxIteration Maximum number of ICP iterations to use in the p2p transformation estimation
+         * @param relativeFitness Threshold for relative fitness to use in the p2p transformation estimation
+         * @param relativeRMSE Threshold for relative RMSE to use in the p2p transformation estimation
+         * @param usePointToPlane Use point-to-plane ICP instead of point-to-point. This replaces the p2p with the point-to-plane transformation estimation
          * @return diffCheck::transformation::DFTransformation
         */
         static diffCheck::transformation::DFTransformation O3DICP(
             std::shared_ptr<geometry::DFPointCloud> source, 
             std::shared_ptr<geometry::DFPointCloud> target,
-            double maxCorrespondenceDistance = 0.01);
+            double maxCorrespondenceDistance = 0.01,
+            bool scalingForPointToPointTransformationEstimation = false,
+            double relativeFitness = 1e-6,
+            double relativeRMSE = 1e-6,
+            int maxIteration = 30,
+            bool usePointToPlane = false);
 
         /**
          * @brief Perform Generalized ICP registration using Open3D
@@ -27,11 +37,17 @@ namespace diffCheck::registration
          * @param source DFPointCloud source point cloud
          * @param target DFPointCloud Target point cloud
          * @param maxCorrespondenceDistance Maximum relative correspondence distance. 0.01 means 1% of the bounding box diagonal
+         * @param maxIteration Maximum number of ICP iterations to use in the p2p transformation estimation
+         * @param relativeFitness Threshold for relative fitness to use in the p2p transformation estimation
+         * @param relativeRMSE Threshold for relative RMSE to use in the p2p transformation estimation
          * @return diffCheck::transformation::DFTransformation
          */
         static diffCheck::transformation::DFTransformation O3DGeneralizedICP(
             std::shared_ptr<geometry::DFPointCloud> source, 
             std::shared_ptr<geometry::DFPointCloud> target,
-            double maxCorrespondenceDistance = 0.01);
+            double maxCorrespondenceDistance = 0.01,
+            int maxIteration = 30,
+            double relativeFitness = 1e-6,
+            double relativeRMSE = 1e-6);
     };
 }
