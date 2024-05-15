6e301b2727295be948f98357ef564e70df8a435a
eleniv3d <43600924+eleniv3d@users.noreply.github.com>
2024-05-15 19:32:36 +0200

    FIX code.py for Mesh and PCD Loader

--- src/gh/components/DF_load_mesh_from_file/code.py
+++ src/gh/components/DF_load_mesh_from_file/code.py
@@ -9,46 +9,33 @@ from ghpythonlib.componentbase import executingcomponent as component
 import Grasshopper as gh
 from Grasshopper.Kernel import GH_RuntimeMessageLevel as RML
 
-import diffCheck
 from diffCheck import diffcheck_bindings
-import diffCheck.df_geometries
-import diffCheck.df_cvt_bindings
-
+from diffCheck import df_cvt_bindings
 
 class DFLoadMeshFromFile(component):
     def RunScript(self,
-        i_path : str,
-        i_scalef : float
-    ) -> rg.PointCloud:
+        i_path: str,
+        i_scalef: float) -> rg.Mesh:
         """
-            This compoonent load a mesh rhino from a ply file.
+            This compoonent loads a Rhino mesh from a .ply file.
 
-            :param i_path: path to the ply file
+            :param i_path: path to the .ply file
             :param i_scalef: scale factor
 
-            :return o_mesh: rhino mesh
+            :return o_mesh: Rhino Mesh
         """
-        print(f"diffCheck version: {diffCheck.__version__}")
-
-        df_cloud = diffcheck_bindings.dfb_geometry.DFPointCloud()
-        df_cloud.load_from_PLY(i_path)
-        rgpoints = [rg.Point3d(pt[0], pt[1], pt[2]) for pt in df_cloud.points]
-        rh_cloud = rg.PointCloud(rgpoints)
+        # import and convert to Rhino Mesh
+        df_mesh = diffcheck_bindings.dfb_geometry.DFMesh()
+        df_mesh.load_from_PLY(i_path)
+        rh_mesh = df_cvt_bindings.cvt_dfmesh_2_rhmesh(df_mesh)
 
         # scale  if needed
-        centroid = rh_cloud.GetBoundingBox(True).Center
+        centroid = rh_mesh.GetBoundingBox(True).Center
         x_form_scale = rg.Transform.Scale(centroid, i_scalef)
-        rh_cloud.Transform(x_form_scale)
+        rh_mesh.Transform(x_form_scale)
 
+        return [rh_mesh]
 
-        rh_mesh = rh_cloud
-
-        return rh_mesh
-
-
-# if __name__ == "__main__":
-#     com = DFLoadMeshFromFile()
-#     o_rh_mesh = com.RunScript(
-#         i_path,n
-#         i_scalef
-#     )
+if __name__ == "__main__":
+    com = DFLoadMeshFromFile()
+    o_rh_mesh = com.RunScript(i_path, i_scalef)
