1902f16c5069972d9dc5b6e15906d3548db51aca
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2024-06-23 13:19:16 +0200

    FIX: code.py

--- src/gh/components/DF_scan_segmentation/code.py
+++ src/gh/components/DF_scan_segmentation/code.py
@@ -3,7 +3,7 @@ import diffCheck.df_cvt_bindings as df_cvt
 import diffCheck.df_geometries as df_geo
 import diffCheck.diffcheck_bindings as df_bindings
 
-def main(model, voxel_size, normal_threshold, min_cluster_size):
+def main(model, voxel_size, normal_threshold, min_cluster_size, knn_neighborhood_size):
     a = []
     df_scan = df_cvt.cvt_rhcloud_2_dfcloud(scan)
     res = df_bindings.dfb_segmentation.DFSegmentation.segmentation_point_cloud(df_scan, voxel_size, normal_threshold, min_cluster_size, false, 10, knn_neighborhood_size)
