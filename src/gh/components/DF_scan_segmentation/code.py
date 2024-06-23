6c95c01f79fe5dc3bfd9d0778a00d17eb9dfbfa7
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2024-06-23 13:18:51 +0200

    FIX: parameters passed to segmentation function in code.py

--- src/gh/components/DF_scan_segmentation/code.py
+++ src/gh/components/DF_scan_segmentation/code.py
@@ -6,11 +6,12 @@ import diffCheck.diffcheck_bindings as df_bindings
 def main(model, voxel_size, normal_threshold, min_cluster_size):
     a = []
     df_scan = df_cvt.cvt_rhcloud_2_dfcloud(scan)
-    res = df_bindings.dfb_segmentation.DFSegmentation.segmentation_point_cloud(df_scan, voxel_size, normal_threshold, min_cluster_size)
+    res = df_bindings.dfb_segmentation.DFSegmentation.segmentation_point_cloud(df_scan, voxel_size, normal_threshold, min_cluster_size, false, 10, knn_neighborhood_size)
     print(len(res))
     for pc in res:
         a.append(pc)
     return a
 
 if __name__ == "__main__":
-    a = main(model, voxel_size, normal_threshold, min_cluster_size)
+    a = main(model, voxel_size, normal_threshold, min_cluster_size, knn_neighborhood_size)
+    
