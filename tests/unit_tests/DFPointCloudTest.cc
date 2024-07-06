d1ab95487b3c69b6ee789c98696193d7b6f6ff25
Andrea Settimi <andreasettimi39@gmail.com>
2024-07-06 23:49:57 +0200

    WIP: working on tests, structure in

--- tests/unit_tests/DFPointCloudTest.cc
+++ tests/unit_tests/DFPointCloudTest.cc
@@ -1,21 +1,55 @@
 #include <gtest/gtest.h>
 #include "diffCheck.hh"
 
+class DFPointCloudTestFixture : public ::testing::Test {
+protected:
+    std::vector<Eigen::Vector3d> points;
+    std::vector<Eigen::Vector3d> colors;
+    std::vector<Eigen::Vector3d> normals;
+    diffCheck::geometry::DFPointCloud dfPointCloud;
 
-TEST(DFPointCloudTest, TestConstructor) {
-    std::vector<Eigen::Vector3d> points = {Eigen::Vector3d(1, 2, 3)};
-    std::vector<Eigen::Vector3d> colors = {Eigen::Vector3d(255, 255, 255)};
-    std::vector<Eigen::Vector3d> normals = {Eigen::Vector3d(0, 0, 1)};
+    DFPointCloudTestFixture() : dfPointCloud(points, colors, normals) {}
 
-    diffCheck::geometry::DFPointCloud dfPointCloud(points, colors, normals);
+    void SetUp() override {
+        // Initialize your objects and variables here
+        points = {Eigen::Vector3d(1, 2, 3)};
+        colors = {Eigen::Vector3d(255, 255, 255)};
+        normals = {Eigen::Vector3d(0, 0, 1)};
 
-    // Verify that the points, colors, and normals are set correctly
-    EXPECT_EQ(dfPointCloud.Points[0], points[0]);
-    EXPECT_EQ(dfPointCloud.Colors[0], colors[0]);
-    EXPECT_EQ(dfPointCloud.Normals[0], normals[0]);
+        // Reinitialize dfPointCloud in case you need to reset its state
+        dfPointCloud = diffCheck::geometry::DFPointCloud(points, colors, normals);
+    }
+
+    void TearDown() override {
+        // Clean up any resources if needed
+    }
+};
+
+TEST_F(DFPointCloudTestFixture, GetNumPoints) {
+    EXPECT_EQ(dfPointCloud.GetNumPoints(), 1);
 }
 
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+TEST_F(DFPointCloudTestFixture, GetNumColors) {
+    EXPECT_EQ(dfPointCloud.GetNumColors(), 1);
 }
+
+TEST_F(DFPointCloudTestFixture, GetNumNormals) {
+    EXPECT_EQ(dfPointCloud.GetNumNormals(), 1);
+}
+
+TEST_F(DFPointCloudTestFixture, HasPoints) {
+    EXPECT_TRUE(dfPointCloud.HasPoints());
+}
+
+TEST_F(DFPointCloudTestFixture, HasColors) {
+    EXPECT_TRUE(dfPointCloud.HasColors());
+}
+
+TEST_F(DFPointCloudTestFixture, HasNormals) {
+    EXPECT_TRUE(dfPointCloud.HasNormals());
+}
+
+// int main(int argc, char **argv) {
+//     ::testing::InitGoogleTest(&argc, argv);
+//     return RUN_ALL_TESTS();
+// }
