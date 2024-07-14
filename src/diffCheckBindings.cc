1dba340d1032a8450cf222ad7445ba6c17762bae
Andrea Settimi <andreasettimi39@gmail.com>
2024-07-14 12:12:57 +0200

    WIP: working on wrap cad segmentator integration

--- src/diffCheckBindings.cc
+++ src/diffCheckBindings.cc
@@ -179,11 +179,13 @@ PYBIND11_MODULE(diffcheck_bindings, m) {
             py::arg("knn_neighborhood_size") = 10,
             py::arg("radius_neighborhood_size") = 0.1,
             py::arg("color_clusters") = false)
+        
         .def_static("associate_clusters", &diffCheck::segmentation::DFSegmentation::AssociateClustersToMeshes,
             py::arg("reference_mesh"),
             py::arg("unassociated_clusters"),
             py::arg("angle_threshold") = 0.1,
             py::arg("association_threshold") = 0.1)
+        
         .def_static("clean_unassociated_clusters", &diffCheck::segmentation::DFSegmentation::CleanUnassociatedClusters,
             py::arg("unassociated_clusters"),
             py::arg("associated_clusters"),
--- src/gh/components/DF_CAD_segmentator/code.py
+++ src/gh/components/DF_CAD_segmentator/code.py
@@ -16,6 +16,8 @@ from diffCheck.diffcheck_bindings import dfb_segmentation
 
 from diffCheck import df_cvt_bindings
 
+import numpy as np
+
 
 class DFCADSegmentator(component):
     def RunScript(self,
@@ -37,18 +39,36 @@ class DFCADSegmentator(component):
         df_clouds = [df_cvt_bindings.cvt_rhcloud_2_dfcloud(cloud) for cloud in i_clouds]
         # df_meshes = [df_cvt_bindings.cvt_rhmesh_2_dfmesh(mesh) for mesh in i_meshes]
 
-        # # get the point clouds corresponding to the beams
-        # df_asssociated_clusters : List[dfCloud] = dfb_segmentation.DFSegmentation.associate_clusters(
-        #     reference_mesh=df_meshes,
-        #     unassociated_clusters=df_clouds,
-        #     angle_threshold=i_angle_threshold,
-        #     association_threshold=i_association_threshold
-        # )
+        df_beams = i_assembly.beams
+        rh_beams_meshes = []
+        for df_b in df_beams:
+            #TODO: check this function to have a better triangluation of the mesh with the parameters
+            rh_b_mesh_faces = [df_b_f.to_mesh() for df_b_f in df_b.side_faces]
+            # rh_b_mesh_faces = [df_b_f.to_brep_face() for df_b_f in df_b.side_faces]
 
-        print("test")
+            # df_b_mesh_faces = [df_cvt_bindings.cvt_rhmesh_2_dfmesh(rh_b_mesh_face) for rh_b_mesh_face in rh_b_mesh_faces]
+            
+            
+            # _ = [rh_beams_meshes.append(m) for m in rh_b_mesh_faces]
 
+            for m in rh_b_mesh_faces:
+                # print(type(m))
+                rh_beams_meshes.append(m)
 
-        return o_clusters
+            # # convert df_b_mesh_faces to numpy array
+            # # get the point clouds corresponding to the beams
+            # df_asssociated_clusters : List[dfCloud] = dfb_segmentation.DFSegmentation.associate_clusters(
+            #     reference_mesh=df_b_mesh_faces,
+            #     unassociated_clusters=df_clouds,
+            #     angle_threshold=i_angle_threshold,
+            #     association_threshold=i_association_threshold
+            # )
+
+            # break
+
+        print("test2")
+
+        return rh_beams_meshes
 
 if __name__ == "__main__":
     com = DFCADSegmentator()
