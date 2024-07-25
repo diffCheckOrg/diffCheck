62095ebf9e72292c788de4ab2a955c1c5c797348
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2024-07-25 14:23:51 +0200

    ADD: code.py to DF_joint_segmentator component

--- src/gh/components/DF_joint_segmentator/code.py
+++ src/gh/components/DF_joint_segmentator/code.py
@@ -1,17 +1,44 @@
+#! python3
+
 import Rhino
 import diffCheck
 from diffCheck import diffcheck_bindings
 from diffCheck import df_cvt_bindings as df_cvt
 import diffCheck.df_util
+import scriptcontext as sc
 
-def main():
-    # assume two inputs: i_cloud and i_joints. i_cloud is the point cloud of the whole object, i_joints is a list of list the meshes of the joints. [[face_1_joint_A, face_2_joint_A, ...], [face_1_joint_B, face_2_joint_B, ...], ...]
-    # segment the joints from the i_cloud and store them in a list that follows the same structure as i_joints: [joint_A, joint_B, ...]
-    # for each joint point cloud, perform a registration by populating the corresponding mesh faces of each joint
-    # apply the registration to the segmented joint point cloud and re-segment the joints
-    # the result is a list of list of the segmented joints, where each segment is locally registered to the corresponding mesh faces: [[face_1_joint_A_arfter_reg_a, face_2_joint_A_after_reg_a, ...], [face_1_joint_B_after_reg_b, face_2_joint_B_after_reg_b, ...], ...]
-    # that way we can analyse the joints individually (face 1 of joint A is too much to the left, ...)
-    pass
+ABSTOL = Rhino.RhinoDoc.ActiveDoc.ModelAbsoluteTolerance
+
+def main(i_clusters, i_faces, i_joint_ids):
+    n_joints = max(i_joint_ids) + 1
+    joints = [ [] for i in range(n_joints) ]
+    for face, id in zip(i_faces, i_joint_ids):
+        face.Subdivide()
+        face.Faces.ConvertQuadsToTriangles()
+        joints[id].append(df_cvt.cvt_rhmesh_2_dfmesh(face))
+    joint_clouds = []
+    registrastions = []
+    joint_segments = []
+    df_clouds = [df_cvt.cvt_rhcloud_2_dfcloud(cluster) for cluster in i_clusters]
+
+    for joint in joints:
+        joint_cloud = diffcheck_bindings.dfb_geometry.DFPointCloud()
+        for face in joint:
+            face_cloud = face.sample_points_uniformly(1000)
+            face_cloud.estimate_normals(knn=30)
+            joint_cloud.add_points(face_cloud)
+        joint_clouds.append(df_cvt.cvt_dfcloud_2_rhcloud(joint_cloud))
+        segment = diffcheck_bindings.dfb_segmentation.DFSegmentation.associate_clusters(joint, df_clouds )
+        diffcheck_bindings.dfb_segmentation.DFSegmentation.clean_unassociated_clusters(df_clouds, [segment], [joint], 0.1, 0.02)
+        registration = diffcheck_bindings.dfb_registrations.DFRefinedRegistration.O3DICP(segment, joint_cloud)
+        res = registration.transformation_matrix
+        registrastions.append(res)
+        joint_segments.append(df_cvt.cvt_dfcloud_2_rhcloud(segment))
+
+    return joint_segments, joint_clouds, registrastions
 
 if __name__ == "__main__":
-    a = main()
+    joint_segments, joint_clouds, registrastions = main(i_clusters, i_faces, i_joint_ids)
+
+    for i in range(len(joint_segments)):
+        joint_segments[i].Transform(df_cvt.cvt_ndarray_2_rh_transform(registrastions[i]))
