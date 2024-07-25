e8119d492f4b11bfc3a592c8aa33f9c192538545
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2024-07-25 18:03:32 +0200

    UPDATE-CAP: component code cleaned, defensive checks added, and metadata updated

--- src/gh/components/DF_joint_segmentator/code.py
+++ src/gh/components/DF_joint_segmentator/code.py
@@ -9,36 +9,62 @@ import scriptcontext as sc
 
 ABSTOL = Rhino.RhinoDoc.ActiveDoc.ModelAbsoluteTolerance
 
-def main(i_clusters, i_faces, i_joint_ids):
+def main(i_clusters, i_joints, i_joint_ids):
+
+    if len(i_joints) != len(i_joint_ids):
+        raise ValueError("The number of joints and joint ids must be the same.")
+    
+    if len(i_clusters) == 0:
+        raise ValueError("No clusters given.")
+
+    if not isinstance(i_clusters[0], Rhino.Geometry.PointCloud):
+        raise ValueError("The input clusters must be PointClouds.")
+    
+    if not isinstance(i_joints[0], Rhino.Geometry.Mesh):
+        raise ValueError("The input joints must be convertible to Meshes.")
+        
+
+    # prepping the reference meshes
     n_joints = max(i_joint_ids) + 1
     joints = [ [] for i in range(n_joints) ]
-    for face, id in zip(i_faces, i_joint_ids):
+    for face, id in zip(i_joints, i_joint_ids):
         face.Subdivide()
         face.Faces.ConvertQuadsToTriangles()
         joints[id].append(df_cvt.cvt_rhmesh_2_dfmesh(face))
+
     joint_clouds = []
-    registrastions = []
+    registrations = []
     joint_segments = []
     df_clouds = [df_cvt.cvt_rhcloud_2_dfcloud(cluster) for cluster in i_clusters]
 
+    # for each joint, find the corresponding clusters and merge them, generate a reference point cloud, and register the merged clusters to the reference point cloud
     for joint in joints:
+
+        # create the reference point cloud
         joint_cloud = diffcheck_bindings.dfb_geometry.DFPointCloud()
+
         for face in joint:
             face_cloud = face.sample_points_uniformly(1000)
             face_cloud.estimate_normals(knn=30)
             joint_cloud.add_points(face_cloud)
+
         joint_clouds.append(df_cvt.cvt_dfcloud_2_rhcloud(joint_cloud))
+
+        # find the corresponding clusters and merge them
         segment = diffcheck_bindings.dfb_segmentation.DFSegmentation.associate_clusters(joint, df_clouds )
         diffcheck_bindings.dfb_segmentation.DFSegmentation.clean_unassociated_clusters(df_clouds, [segment], [joint], 0.1, 0.02)
+
+        # register the merged clusters to the reference point cloud
         registration = diffcheck_bindings.dfb_registrations.DFRefinedRegistration.O3DICP(segment, joint_cloud)
         res = registration.transformation_matrix
-        registrastions.append(res)
+
+        registrations.append(df_cvt.cvt_ndarray_2_rh_transform(res))
         joint_segments.append(df_cvt.cvt_dfcloud_2_rhcloud(segment))
 
-    return joint_segments, joint_clouds, registrastions
+    return joint_segments, joint_clouds, registrations
 
 if __name__ == "__main__":
-    joint_segments, joint_clouds, registrastions = main(i_clusters, i_faces, i_joint_ids)
+    o_joint_segments, o_reference_point_clouds, o_transforms = main(i_clusters, i_joints, i_joint_ids)
 
-    for i in range(len(joint_segments)):
-        joint_segments[i].Transform(df_cvt.cvt_ndarray_2_rh_transform(registrastions[i]))
+    for i in range(len(o_joint_segments)):
+        o_joint_segments[i].Transform(o_transforms[i])
