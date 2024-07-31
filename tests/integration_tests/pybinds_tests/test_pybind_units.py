b66ee8c406125cbfb11122ac109f63cce7b67503
Andrea Settimi <andreasettimi39@gmail.com>
2024-07-31 12:05:12 +0200

    WIP: integrating the python test full suite

--- tests/integration_tests/pybinds_tests/test_pybind_units.py
+++ tests/integration_tests/pybinds_tests/test_pybind_units.py
@@ -49,86 +49,86 @@ def test_DFPointCloud_load_from_PLY():
     assert pc.normals.__len__() == 7379, "DFPointCloud should have 7379 normals"
     assert pc.colors.__len__() == 7379, "DFPointCloud should have 7379 colors"
 
-# @pytest.fixture
-# def create_DFPointCloudSampleRoof():
-#     df_pcd = dfb.dfb_geometry.DFPointCloud()
-#     df_pcd.load_from_PLY(get_ply_cloud_roof_quarter_path())
-#     yield df_pcd
+@pytest.fixture
+def create_DFPointCloudSampleRoof():
+    df_pcd = dfb.dfb_geometry.DFPointCloud()
+    df_pcd.load_from_PLY(get_ply_cloud_roof_quarter_path())
+    yield df_pcd
 
-# def test_DFPointCloud_properties(create_DFPointCloudSampleRoof):
-#     pc = create_DFPointCloudSampleRoof
-#     assert pc.points.__len__() == 7379, "DFPointCloud should have 7379 points"
-#     assert pc.normals.__len__() == 7379, "DFPointCloud should have 7379 normals"
-#     assert pc.colors.__len__() == 7379, "DFPointCloud should have 7379 colors"
+def test_DFPointCloud_properties(create_DFPointCloudSampleRoof):
+    pc = create_DFPointCloudSampleRoof
+    assert pc.points.__len__() == 7379, "DFPointCloud should have 7379 points"
+    assert pc.normals.__len__() == 7379, "DFPointCloud should have 7379 normals"
+    assert pc.colors.__len__() == 7379, "DFPointCloud should have 7379 colors"
 
-#     assert pc.get_num_points() == 7379, "get_num_points() should return 7379"
-#     assert pc.get_num_normals() == 7379, "get_num_normals() should return 7379"
-#     assert pc.get_num_colors() == 7379, "get_num_colors() should return 7379"
+    assert pc.get_num_points() == 7379, "get_num_points() should return 7379"
+    assert pc.get_num_normals() == 7379, "get_num_normals() should return 7379"
+    assert pc.get_num_colors() == 7379, "get_num_colors() should return 7379"
 
-#     assert pc.has_points() == True, "has_points() should return True"
-#     assert pc.has_colors() == True, "has_colors() should return True"
-#     assert pc.has_normals() == True, "has_normals() should return True"
+    assert pc.has_points() == True, "has_points() should return True"
+    assert pc.has_colors() == True, "has_colors() should return True"
+    assert pc.has_normals() == True, "has_normals() should return True"
 
-#     pc.points = []
-#     pc.normals = []
-#     pc.colors = []
-#     assert pc.has_points() == False, "has_points() should return False"
-#     assert pc.has_colors() == False, "has_colors() should return False"
-#     assert pc.has_normals() == False, "has_normals() should return False"
+    pc.points = []
+    pc.normals = []
+    pc.colors = []
+    assert pc.has_points() == False, "has_points() should return False"
+    assert pc.has_colors() == False, "has_colors() should return False"
+    assert pc.has_normals() == False, "has_normals() should return False"
 
-# def test_DFPointCloud_apply_color(create_DFPointCloudSampleRoof):
-#     pc = create_DFPointCloudSampleRoof
-#     pc.apply_color(255, 0, 0)
-#     for color in pc.colors:
-#         assert (color[0] == 1 and color[1] == 0 and color[2] == 0), "All colors should be (255, 0, 0)"
+def test_DFPointCloud_apply_color(create_DFPointCloudSampleRoof):
+    pc = create_DFPointCloudSampleRoof
+    pc.apply_color(255, 0, 0)
+    for color in pc.colors:
+        assert (color[0] == 1 and color[1] == 0 and color[2] == 0), "All colors should be (255, 0, 0)"
 
-# def test_DFPointCloud_voxel_func(create_DFPointCloudSampleRoof):
-#     pc = create_DFPointCloudSampleRoof
-#     pc.voxel_downsample(0.01)
-#     assert pc.points.__len__() == 7256, "DFPointCloud should have 7256 points"
-#     pc.uniform_downsample(3)
-#     assert pc.points.__len__() == 2419, "DFPointCloud should have 2419 points"
-#     pc.downsample_by_size(1000)
-#     assert pc.points.__len__() == 1000, "DFPointCloud should have 1000 points"
+def test_DFPointCloud_voxel_func(create_DFPointCloudSampleRoof):
+    pc = create_DFPointCloudSampleRoof
+    pc.voxel_downsample(0.01)
+    assert pc.points.__len__() == 7256, "DFPointCloud should have 7256 points"
+    pc.uniform_downsample(3)
+    assert pc.points.__len__() == 2419, "DFPointCloud should have 2419 points"
+    pc.downsample_by_size(1000)
+    assert pc.points.__len__() == 1000, "DFPointCloud should have 1000 points"
 
-# def test_DFPointCloud_compute_normals(create_DFPointCloudSampleRoof):
-#     pc = create_DFPointCloudSampleRoof
-#     pc.normals.clear()
-#     pc.estimate_normals()
-#     assert pc.normals.__len__() == 7379, "DFPointCloud should have 7379 normals"
+def test_DFPointCloud_compute_normals(create_DFPointCloudSampleRoof):
+    pc = create_DFPointCloudSampleRoof
+    pc.normals.clear()
+    pc.estimate_normals()
+    assert pc.normals.__len__() == 7379, "DFPointCloud should have 7379 normals"
 
-# def test_DFPointCloud_get_tight_bounding_box(create_DFPointCloudSampleRoof):
-#     pc = create_DFPointCloudSampleRoof
-#     obb = pc.get_tight_bounding_box()
-#     assert obb[0][0] == 0.1955558282162114, "The min x of the OBB should be 0.1955558282162114"
+def test_DFPointCloud_get_tight_bounding_box(create_DFPointCloudSampleRoof):
+    pc = create_DFPointCloudSampleRoof
+    obb = pc.get_tight_bounding_box()
+    assert obb[0][0] == 0.1955558282162114, "The min x of the OBB should be 0.1955558282162114"
 
-# # TODO: to implement DFMesh tests
-# def test_DFMesh_init():
-#     pass
+# TODO: to implement DFMesh tests
+def test_DFMesh_init():
+    pass
 
-# #------------------------------------------------------------------------------
-# # dfb_transformation namespace
-# #------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+# dfb_transformation namespace
+#------------------------------------------------------------------------------
 
-# def test_DFTransform_init():
-#     t = dfb.dfb_transformation.DFTransformation()
-#     assert t is not None, "DFTransformation should be initialized successfully"
+def test_DFTransform_init():
+    t = dfb.dfb_transformation.DFTransformation()
+    assert t is not None, "DFTransformation should be initialized successfully"
 
-# def test_DFTransform_read_write(create_DFPointCloudSampleRoof):
-#     pc = create_DFPointCloudSampleRoof
-#     t = dfb.dfb_transformation.DFTransformation()
+def test_DFTransform_read_write(create_DFPointCloudSampleRoof):
+    pc = create_DFPointCloudSampleRoof
+    t = dfb.dfb_transformation.DFTransformation()
 
-#     matrix = t.transformation_matrix
-#     print(matrix)
-#     assert matrix is not None, "Transformation matrix should be initialized"
+    matrix = t.transformation_matrix
+    print(matrix)
+    assert matrix is not None, "Transformation matrix should be initialized"
 
-#     matrix_identity = [[1.0, 0.0, 0.0, 0.0],
-#                        [0.0, 1.0, 0.0, 0.0],
-#                        [0.0, 0.0, 1.0, 0.0],
-#                        [0.0, 0.0, 0.0, 1.0]]
+    matrix_identity = [[1.0, 0.0, 0.0, 0.0],
+                       [0.0, 1.0, 0.0, 0.0],
+                       [0.0, 0.0, 1.0, 0.0],
+                       [0.0, 0.0, 0.0, 1.0]]
 
-#     t.transformation_matrix = matrix_identity
-#     assert (t.transformation_matrix == matrix_identity).all(), "Transformation matrix should be set to identity"
+    t.transformation_matrix = matrix_identity
+    assert (t.transformation_matrix == matrix_identity).all(), "Transformation matrix should be set to identity"
 
 
 #------------------------------------------------------------------------------
