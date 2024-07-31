f114f8fbbc862f30c925159ad503f1db46bdeb70
Andrea Settimi <andreasettimi39@gmail.com>
2024-07-31 11:18:24 +0200

    WIP: testing with local ply in same py directory of tests

--- tests/integration_tests/pybinds_tests/test_pybind_units.py
+++ tests/integration_tests/pybinds_tests/test_pybind_units.py
@@ -44,7 +44,7 @@ def test_DFPointCloud_init():
 def test_DFPointCloud_load_from_PLY():
     pc = dfb.dfb_geometry.DFPointCloud()
     # pc.load_from_PLY(get_ply_cloud_roof_quarter_path())
-    pc.load_from_PLY("./roof_quarter.ply")
+    pc.load_from_PLY(os.path.join(os.path.dirname(__file__), "./roof_quarter.ply"))
     
     assert pc.points.__len__() == 7379, "DFPointCloud should have 7379 points"
     assert pc.normals.__len__() == 7379, "DFPointCloud should have 7379 normals"
