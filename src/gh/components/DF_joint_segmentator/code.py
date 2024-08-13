039d6d05676500f9925aa0cdec15bc7f28f3e3b8
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2024-08-13 10:09:56 +0200

    DF_Joint_segmentator refactored with DF_Assembly

--- src/gh/components/DF_joint_segmentator/code.py
+++ src/gh/components/DF_joint_segmentator/code.py
@@ -7,15 +7,19 @@ from diffCheck import diffcheck_bindings
 from diffCheck import df_cvt_bindings as df_cvt
 import diffCheck.df_util
 
+from Grasshopper.Kernel import GH_RuntimeMessageLevel as RML
 from ghpythonlib.componentbase import executingcomponent as component
+
+import typing
 
 ABSTOL = Rhino.RhinoDoc.ActiveDoc.ModelAbsoluteTolerance
 
 class DFJointSegmentator(component):
+    # def __init__(self):
+    #     super(DFJointSegmentator, self).__init__()
     def RunScript(self, 
-                  i_clusters: Rhino.Geometry.PointCloud, 
-                  i_joints: Rhino.Geometry.Mesh, 
-                  i_joint_ids: int,
+                  i_clusters: typing.List[Rhino.Geometry.PointCloud], 
+                  i_assembly: diffCheck.df_geometries.DFAssembly,
                   i_angle_threshold: float,
                   i_distance_threshold: float):
         """
@@ -24,16 +28,12 @@ class DFJointSegmentator(component):
         and returns the joint segments, the reference point clouds, and the ICP transformations from the first to the second.
 
         :param i_clusters: The clusters to be associated to the joints.
-        :param i_joints: The joints to which the clusters will be associated. These are meshes.
-        :param i_joint_ids: The joint ids of the joints.
+        :param i_assembly: The DFAssembly containing the joints we want to segment.
         :param i_angle_threshold: The angle threshold for the association of the clusters to the joints.
         :param i_distance_threshold: The distance threshold for the association of the clusters to the joints.
         """
         if i_angle_threshold is None : i_angle_threshold = 0.1
         if i_distance_threshold is None : i_distance_threshold = 0.1
-
-        if len(i_joints) != len(i_joint_ids):
-            raise ValueError("The number of joints and joint ids must be the same.")
         
         if len(i_clusters) == 0:
             raise ValueError("No clusters given.")
@@ -41,26 +41,29 @@ class DFJointSegmentator(component):
         if not isinstance(i_clusters[0], Rhino.Geometry.PointCloud):
             raise ValueError("The input clusters must be PointClouds.")
         
-        if not isinstance(i_joints[0], Rhino.Geometry.Mesh):
+        all_joints = i_assembly.all_joints
+        if not isinstance(all_joints[0].faces[0].to_mesh(), Rhino.Geometry.Mesh):
             raise ValueError("The input joints must be convertible to Meshes.")
             
+        # get number of joints
+        n_joints = max([joint.id for joint in all_joints]) + 1
 
         # prepping the reference meshes
-        n_joints = max(i_joint_ids) + 1
-        joints = [ [] for i in range(n_joints) ]
-        for face, id in zip(i_joints, i_joint_ids):
-            face.Subdivide()
-            face.Faces.ConvertQuadsToTriangles()
-            joints[id].append(df_cvt.cvt_rhmesh_2_dfmesh(face))
+        joints = [[] for _ in range(n_joints)]
+        for joint in all_joints:
+            for face in joint.faces:
+                face = face.to_mesh()
+                face.Subdivide()
+                face.Faces.ConvertQuadsToTriangles()
+                joints[joint.id].append(df_cvt.cvt_rhmesh_2_dfmesh(face))
 
         joint_clouds = []
-        registrations = []
+        transforms = []
         joint_segments = []
         df_clouds = [df_cvt.cvt_rhcloud_2_dfcloud(cluster) for cluster in i_clusters]
 
         # for each joint, find the corresponding clusters and merge them, generate a reference point cloud, and register the merged clusters to the reference point cloud
         for joint in joints:
-
             # create the reference point cloud
             joint_cloud = diffcheck_bindings.dfb_geometry.DFPointCloud()
 
@@ -72,19 +75,29 @@ class DFJointSegmentator(component):
 
             # find the corresponding clusters and merge them
             segment = diffcheck_bindings.dfb_segmentation.DFSegmentation.associate_clusters(joint, df_clouds, i_angle_threshold, i_distance_threshold)
-            diffcheck_bindings.dfb_segmentation.DFSegmentation.clean_unassociated_clusters(df_clouds, [segment], [joint], i_angle_threshold ,i_distance_threshold)
-
+            diffcheck_bindings.dfb_segmentation.DFSegmentation.clean_unassociated_clusters(df_clouds, [segment], [joint], i_angle_threshold, i_distance_threshold)
+            
             # register the merged clusters to the reference point cloud
             registration = diffcheck_bindings.dfb_registrations.DFRefinedRegistration.O3DICP(segment, joint_cloud)
             res = registration.transformation_matrix
-
-            registrations.append(df_cvt.cvt_ndarray_2_rh_transform(res))
+            transforms.append(df_cvt.cvt_ndarray_2_rh_transform(res))
             joint_segments.append(df_cvt.cvt_dfcloud_2_rhcloud(segment))
+        
+        o_joint_segments = []
+        o_transforms = []
+        o_reference_point_clouds = []
+        for  joint_segment, transform, _joint_cloud in zip(joint_segments, transforms, joint_clouds):
+            if joint_segment.IsValid:
+                o_joint_segments.append(joint_segment)
+                o_transforms.append(transform)
+                o_reference_point_clouds.append(_joint_cloud)
+            else:
+                ghenv.Component.AddRuntimeMessage(RML.Warning, "Some joints could not be segmented and were ignored.")
 
-        return joint_segments, registrations, joint_clouds
+        return o_joint_segments, o_transforms, o_reference_point_clouds
 
 # if __name__ == "__main__":
-#     o_joint_segments, o_reference_point_clouds, o_transforms = main(i_clusters, i_joints, i_joint_ids)
-
+#     comp = DFJointSegmentator()
+#     o_joint_segments, o_transforms, o_reference_point_clouds = comp.RunScript(i_clusters, i_assembly, i_angle_threshold, i_distance_threshold)
 #     for i in range(len(o_joint_segments)):
 #         o_joint_segments[i].Transform(o_transforms[i])
