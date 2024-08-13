3fb136b58334e80a6d5cf59029be028bf761aa68
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2024-08-13 14:33:17 +0200

    UPDATE: missing python unit tests added for pointclouds and meshes

--- tests/integration_tests/pybinds_tests/test_pybind_units.py
+++ tests/integration_tests/pybinds_tests/test_pybind_units.py
@@ -152,6 +152,27 @@ def test_DFPointCloud_get_tight_bounding_box(create_DFPointCloudSampleRoof):
     # round to the 3 decimal places
     assert round(obb[0][0], 3) == 0.196, "The min x of the OBB should be 0.196"
 
+def test_DFPointCloud_get_axis_aligned_bounding_box(create_DFPointCloudSampleRoof):
+    pc = create_DFPointCloudSampleRoof
+    aabb = pc.get_axis_aligned_bounding_box()
+    # round to the 3 decimal places
+    assert round(aabb[0][0], 3) == -2.339, "The min x of the AABB should be 0.196"
+
+def test_DFPointCloud_compute_distance():
+    point_pc_1 = [(0, 0, 0)]
+    point_pc_2 = [(1, 0, 0)]
+    normal_pc_1 = [(0, 0, 1)]
+    normal_pc_2 = [(0, 0, 1)]
+    color_pc_1 = [(0, 0, 0)]
+    color_pc_2 = [(0, 0, 0)]
+    
+    pc_1 = dfb.dfb_geometry.DFPointCloud(point_pc_1, normal_pc_1, color_pc_1)
+    pc_2 = dfb.dfb_geometry.DFPointCloud(point_pc_2, normal_pc_2, color_pc_2)
+
+    distance = pc_1.compute_distance(pc_2)[0]
+
+    assert distance == 1. , "The distance between the two points should be 1."
+
 # mesh tests
 
 def test_DFMesh_init():
@@ -162,6 +183,32 @@ def test_DFMesh_load_from_PLY(create_DFMeshCube):
     mesh = create_DFMeshCube
     assert mesh.vertices.__len__() == 726, "DFMesh should have 726 vertices"
     assert mesh.faces.__len__() == 1200, "DFMesh should have 800 faces"
+
+def test_DFMesh_properties():
+    vertices = [[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0], [0, 1, -1], [0, 0, -1]]
+    faces = [[0, 1, 2], [0, 2, 3], [0, 3, 4], [0, 4, 5]]
+    mesh = dfb.dfb_geometry.DFMesh(vertices, faces, [], [], [])
+    assert mesh.get_num_vertices() == 6, "get_num_vertices() should return 6"
+    assert mesh.get_num_faces() == 4, "get_num_faces() should return 4"
+    assert isinstance(mesh.vertices, list), "vertices should be a list"
+    assert isinstance(mesh.faces, list), "faces should be a list"
+    assert isinstance(mesh.normals_face, list), "normals_faces should be a list"
+    assert isinstance(mesh.normals_vertex, list), "normals_vertex should be a list"
+    assert isinstance(mesh.colors_face, list), "colors_faces should be a list"
+    assert isinstance(mesh.colors_vertex, list), "colors_vertex should be a list"
+    assert len(mesh.vertices[0]) == 3, "vertices should be a list of 3 coordinates"
+    assert len(mesh.faces[0]) == 3, "faces should be a list of 3 indexes"
+
+def test_DFMesh_compute_distance():
+    vertices = [[0, 0, 0], [1, 0, 0], [0, 1, 0]]
+    faces = [[0, 1, 2]]
+    mesh = dfb.dfb_geometry.DFMesh(vertices, faces, [], [], [])
+    point = [(0, 0, 1)]
+    normal = [(0, 0, 1)]
+    color = [(0, 0, 0)]
+    pc = dfb.dfb_geometry.DFPointCloud(point, normal, color)
+    distance = mesh.compute_distance(pc)[0]
+    assert distance == 1.0, "The distance between the point and the mesh should be 1.0"
 
 def test_DFMesh_sample_points(create_DFMeshCube):
     mesh = create_DFMeshCube
