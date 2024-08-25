698fdd2114e8edaf038ef4b6ee2adc0d172feef9
Andrea Settimi <andreasettimi39@gmail.com>
2024-08-25 11:32:05 +0200

    FIX: for building documentation for diffCheck py __init__ and sphinx conf env

--- src/gh/diffCheck/diffCheck/__init__.py
+++ src/gh/diffCheck/diffCheck/__init__.py
@@ -7,6 +7,6 @@ PATH_TO_DLL = "dlls"
 extra_dll_dir = os.path.join(os.path.dirname(__file__), PATH_TO_DLL)
 os.add_dll_directory(extra_dll_dir)
 
-# import the bindings
-from . import diffcheck_bindings  # type: ignore[attr-defined]
-from . import df_cvt_bindings  # type: ignore[attr-defined]
+if not os.getenv('SPHINX_BUILD', False):
+    from . import diffcheck_bindings  # type: ignore[attr-defined]
+    from . import df_cvt_bindings  # type: ignore[attr-defined]
