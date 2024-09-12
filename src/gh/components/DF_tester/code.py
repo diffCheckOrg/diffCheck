732d0f5d2fed6de540142e03a98d090b22928c78
Andrea Settimi <andreasettimi39@gmail.com>
2024-09-12 11:09:17 +0200

    WIP-FIX: message to inform that the workspace should be in meters

--- src/gh/components/DF_tester/code.py
+++ src/gh/components/DF_tester/code.py
@@ -11,14 +11,22 @@ from diffCheck import diffcheck_bindings
 
 class DFTester(component):
     def RunScript(self):
+        # version
         ghenv.Component.Message = f"diffCheck v: {diffCheck.__version__}"  # noqa: F821
-        is_binding_imported = diffcheck_bindings.dfb_test.test()
 
+        # bindings
+        is_binding_imported = diffcheck_bindings.dfb_test.test()
         if not is_binding_imported:
             ghenv.Component.AddRuntimeMessage(RML.Warning, "Bindings not imported.")  # noqa: F821
         else:
             ghenv.Component.AddRuntimeMessage(RML.Remark, "Bindings imported.")  # noqa: F821
-
         print(f"diffCheck test: {diffCheck.df_cvt_bindings.test_bindings()}")
+
+        # workspace unit
+        scalef = diffCheck.df_util.get_doc_2_meters_unitf()
+        if scalef != 1.0:
+            ghenv.Component.AddRuntimeMessage(RML.Warning, "Workspace unit is not in meters.")  # noqa: F821
+        else:
+            ghenv.Component.AddRuntimeMessage(RML.Remark, "Workspace unit is in meters.")  # noqa: F821
 
         return is_binding_imported
