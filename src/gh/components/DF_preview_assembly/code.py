dfbc7053bed61ec124f2abc80a1ef976937b190f
Andrea Settimi <andreasettimi39@gmail.com>
2024-09-19 11:08:03 +0200

    MILESTONE: integrated the bool switch

--- src/gh/components/DF_preview_assembly/code.py
+++ src/gh/components/DF_preview_assembly/code.py
@@ -7,6 +7,7 @@ import typing
 import Rhino.Geometry as rg
 
 from ghpythonlib.componentbase import executingcomponent as component
+import Grasshopper as gh
 
 import diffCheck
 from diffCheck.df_geometries import DFAssembly
@@ -16,15 +17,64 @@ import diffCheck.df_util
 import numpy as np
 
 
+def add_bool_toggle(self,
+    nickname: str,
+    indx: int,
+    X_param_coord: float,
+    Y_param_coord: float,
+    X_offset: int=87
+    ) -> None:
+    """
+        Adds a boolean toggle to the component input
+
+        :param nickname: the nickname of the value list
+        :param indx: the index of the input parameter
+        :param X_param_coord: the x coordinate of the input parameter
+        :param Y_param_coord: the y coordinate of the input parameter
+        :param X_offset: the offset of the value list from the input parameter
+    """
+    param = ghenv.Component.Params.Input[indx]  # noqa: F821
+    if param.SourceCount == 0:
+        toggle = gh.Kernel.Special.GH_BooleanToggle()
+        toggle.NickName = nickname
+        toggle.Description = "Toggle the value to use with DFVizSettings"
+        toggle.CreateAttributes()
+        toggle.Attributes.Pivot = System.Drawing.PointF(
+            X_param_coord - (toggle.Attributes.Bounds.Width) - X_offset,
+            Y_param_coord - (toggle.Attributes.Bounds.Height / 2 + 0.1)
+            )
+        toggle.Attributes.ExpireLayout()
+        gh.Instances.ActiveCanvas.Document.AddObject(toggle, False)
+        ghenv.Component.Params.Input[indx].AddSource(toggle)  # noqa: F821
+
 class DFPreviewAssembly(component):
     def __init__(self):
         super(DFPreviewAssembly, self).__init__()
         self._dfassembly = None
         self._joint_rnd_clr = None
 
-    def RunScript(self, i_assembly: DFAssembly=None):
+        ghenv.Component.ExpireSolution(True)  # noqa: F821
+        ghenv.Component.Attributes.PerformLayout()  # noqa: F821
+        params = getattr(ghenv.Component.Params, "Input")  # noqa: F821
+        for j in range(len(params)):
+            Y_cord = params[j].Attributes.InputGrip.Y + 1
+            X_cord = params[j].Attributes.Pivot.X + 20
+            input_indx = j
+            if "i_are_joints_visible" == params[j].NickName:
+                add_bool_toggle(
+                    ghenv.Component,  # noqa: F821
+                    "show_joints",
+                    input_indx, X_cord, Y_cord)
+
+
+    def RunScript(self,
+        i_assembly: DFAssembly=None,
+        i_are_joints_visible: bool=None
+        ):
         if i_assembly is None:
             return None
+        if i_are_joints_visible is None:
+            i_are_joints_visible = False
 
         self._dfassembly = i_assembly
 
@@ -32,6 +82,9 @@ class DFPreviewAssembly(component):
             System.Convert.ToInt32(255 * np.random.rand()),
             System.Convert.ToInt32(255 * np.random.rand()),
             System.Convert.ToInt32(255 * np.random.rand())) for _ in range(len(self._dfassembly.beams))]
+
+        self._are_joints_visible = i_are_joints_visible
+
         return None
 
     # Preview overrides
@@ -67,31 +120,24 @@ class DFPreviewAssembly(component):
             #######################################
             ## DFJoints
             #######################################
-            # draw an hatch for each joint's face
-            # beam_center = beam.center
-            # beam_idx
-            for idx_joint, joint in enumerate(beam.joints):
-                joint_faces = joint.faces
-                for idx_face, face in enumerate(joint_faces):
-                    face_center = face.to_brep_face().GetBoundingBox(False).Center
-                    args.Display.DrawPoint(face_center, self._joint_rnd_clr[idx_joint])
+            if self._are_joints_visible:
+                for idx_joint, joint in enumerate(beam.joints):
+                    joint_faces = joint.faces
+                    for idx_face, face in enumerate(joint_faces):
+                        face_center = face.to_brep_face().GetBoundingBox(False).Center
+                        args.Display.DrawPoint(face_center, self._joint_rnd_clr[idx_joint])
 
-                    vector_face_center_2_beam_center = face_center - beam.center
-                    vector_face_center_2_beam_center.Unitize()
-                    vector_face_center_2_beam_center *= 0.4 * extension_length
+                        vector_face_center_2_beam_center = face_center - beam.center
+                        vector_face_center_2_beam_center.Unitize()
+                        vector_face_center_2_beam_center *= 0.4 * extension_length
 
-                    ln = rg.Line(face_center, face_center + vector_face_center_2_beam_center)
-                    args.Display.DrawDottedLine(ln, self._joint_rnd_clr[idx_joint])
+                        ln = rg.Line(face_center, face_center + vector_face_center_2_beam_center)
+                        args.Display.DrawDottedLine(ln, self._joint_rnd_clr[idx_joint])
 
-                    # name of the joint is defined by: 1) the beam index, 2) the joint index, 3) the face index
-                    name_face_joint: str = f"{beam.index_assembly}-{idx_joint}-{idx_face}"
-                    args.Display.Draw2dText(
-                        name_face_joint,
-                        self._joint_rnd_clr[idx_joint],
-                        ln.To,
-                        True, 18)
-                # joint_faces = joint.faces
-                # for face in joint_faces:
-                #     mesh_rh = face.to_mesh()
-
-                #     args.Display.DrawMeshWires(mesh_rh, System.Drawing.Color.Blue)
+                        # name of the joint is defined by: 1) the beam index, 2) the joint index, 3) the face index by list order
+                        name_face_joint: str = f"{beam.index_assembly}-{joint.id}-{idx_face}"
+                        args.Display.Draw2dText(
+                            name_face_joint,
+                            self._joint_rnd_clr[idx_joint],
+                            ln.To,
+                            True, 18)
