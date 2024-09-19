cc37b5021f135bc416b6874dc3e7d89e206f0f9a
Andrea Settimi <andreasettimi39@gmail.com>
2024-09-19 22:17:35 +0200

    MILESTONE: release publish ci ready

--- src/gh/diffCheck/setup.py
+++ src/gh/diffCheck/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="diffCheck",
-    version="0.0.31",
+    version="0.0.32",
     packages=find_packages(),
     install_requires=[
         "numpy",
