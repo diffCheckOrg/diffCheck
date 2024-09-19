1c48010a98b4c9a826782b6e1184020b5fee8e96
Andrea Settimi <andreasettimi39@gmail.com>
2024-09-19 16:20:34 +0200

    WIP: workin on ci - canoot be tested with act locally since win x10

--- src/gh/diffCheck/setup.py
+++ src/gh/diffCheck/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="diffCheck",
-    version="0.0.26",
+    version="0.0.29",
     packages=find_packages(),
     install_requires=[
         "numpy",
