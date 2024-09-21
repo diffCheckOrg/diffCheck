abe3f618bbd9a4497b834edce5feee8006398e27
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2024-09-21 15:38:10 +0200

    UPDATE-WIP: add is_cylinder parameter to BuildAssembly component

--- src/gh/components/DF_build_assembly/code.py
+++ src/gh/components/DF_build_assembly/code.py
@@ -13,10 +13,15 @@ from diffCheck.df_geometries import DFBeam, DFAssembly
 class DFBuildAssembly(component):
     def RunScript(self,
             i_assembly_name,
-            i_breps : System.Collections.Generic.IList[Rhino.Geometry.Brep]):
+            i_breps : System.Collections.Generic.IList[Rhino.Geometry.Brep],
+            i_is_cylinder : bool):
         beams: typing.List[DFBeam] = []
+        if i_is_cylinder is None:
+            i_is_cylinder = False
+
         for brep in i_breps:
             beam = DFBeam.from_brep_face(brep)
+            beam.is_cylinder = i_is_cylinder
             beams.append(beam)
 
         o_assembly = DFAssembly(beams, i_assembly_name)
