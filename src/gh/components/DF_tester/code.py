cd026439eff09b688ee8e800190d8622884d2fed
eleniv3d <43600924+eleniv3d@users.noreply.github.com>
2024-09-22 10:15:55 +0200

    FIX undo changes ro DF_Tester component

--- src/gh/components/DF_tester/code.py
+++ src/gh/components/DF_tester/code.py
@@ -9,94 +9,24 @@ import diffCheck
 import diffCheck.df_cvt_bindings
 from diffCheck import diffcheck_bindings
 
-
-from ghpythonlib.componentbase import executingcomponent as component
-from System.Windows.Forms import ToolStripSeparator
-import Grasshopper, GhPython
-import System
-import Rhino
-import rhinoscriptsyntax as rs
-import math
-import fractions
-
 class DFTester(component):
-    def __init__(self):
-        super(DFTester,self).__init__()
-        self.factor = 1
-        self.checked1 = False
-        self.checked2 = False
-        self.checked3 = False
+    def RunScript(self):
+        # version
+        ghenv.Component.Message = f"diffCheck v: {diffCheck.__version__}"  # noqa: F821
 
-    def RunScript(self, decIn):
-        if self.factor == 1: 
-            ftIn = decIn
-            self.Message = "Make a Choice"
-        else: ftIn = self.feet_inches(decIn)
-        return ftIn
+        # bindings
+        is_binding_imported = diffcheck_bindings.dfb_test.test()
+        if not is_binding_imported:
+            ghenv.Component.AddRuntimeMessage(RML.Warning, "Bindings not imported.")  # noqa: F821
+        else:
+            ghenv.Component.AddRuntimeMessage(RML.Remark, "Bindings imported.")  # noqa: F821
+        print(f"diffCheck test: {diffCheck.df_cvt_bindings.test_bindings()}")
 
-    def feet_inches(self, decimal):
-        tol = round(decimal * self.factor)
-        b = math.modf(tol / self.factor)
-        feet = int(b[1] // 12)
-        inch = int(b[1] % 12)
-        fract = fractions.Fraction(b[0])
-        if fract == 0:
-            fract = ""
-        fi = str(feet)+"'- "+str(inch)+" "+str(fract)+'"'
-        return fi
+        # workspace unit
+        scalef = diffCheck.df_util.get_doc_2_meters_unitf()
+        if scalef != 1.0:
+            ghenv.Component.AddRuntimeMessage(RML.Warning, "Workspace unit is not in meters.")  # noqa: F821
+        else:
+            ghenv.Component.AddRuntimeMessage(RML.Remark, "Workspace unit is in meters.")  # noqa: F821
 
-    def AppendAdditionalComponentMenuItems(self, items):
-        try: #always everything inside try
-            #context menu item 1
-            component.AppendAdditionalMenuItems(self, items)
-            image = None
-            items.Items.Add(ToolStripSeparator())
-            item = items.Items.Add("Round to 1/2", image, self.OnClicked1)
-            item.ToolTipText = "Round 1/2"
-            item.Name = "2"
-            item.Checked = self.checked1
-            #context menu item 2
-            item = items.Items.Add("Round to 1/4", image, self.OnClicked2)
-            item.ToolTipText = "Round 1/4 "
-            item.Name = "4"
-            item.Checked = self.checked2
-            #context menu item 3
-            item = items.Items.Add("Round to 1/8", image, self.OnClicked3)
-            item.ToolTipText = "Round 1/8"
-            item.Name = "8"
-            item.Checked = self.checked3
-        except Exception as ex:
-            System.Windows.Forms.MessageBox.Show(str(ex))
-            
-    def OnClicked1(self, index, value):
-        try: #always everything inside try
-            self.checked1 = not self.checked1
-            self.checked2 = False
-            self.checked3 = False
-            self.factor = int(index.Name)
-            self.Message = (index.ToolTipText)
-            self.ExpireSolution(True)
-        except Exception as ex:
-            System.Windows.Forms.MessageBox.Show(str(ex))
-    
-    def OnClicked2(self, index, value):
-        try: #always everything inside try
-            self.checked2 = not self.checked2
-            self.checked1 = False
-            self.checked3 = False
-            self.factor = int(index.Name)
-            self.Message = (index.ToolTipText)
-            self.ExpireSolution(True)
-        except Exception as ex:
-            System.Windows.Forms.MessageBox.Show(str(ex))
-            
-    def OnClicked3(self, index, value):
-        try: #always everything inside try
-            self.checked3 = not self.checked3
-            self.checked1 = False
-            self.checked2 = False
-            self.factor = int(index.Name)
-            self.Message = (index.ToolTipText)
-            self.ExpireSolution(True)
-        except Exception as ex:
-            System.Windows.Forms.MessageBox.Show(str(ex))
+        return is_binding_imported
