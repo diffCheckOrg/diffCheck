1f1e22e6fd72bd75b07d2fdda844ba64a0344a53
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2024-09-23 11:26:52 +0200

    FIX: naming of functions and switch from 'is_cylinder' to 'is_roundwood'

--- src/gh/diffCheck/diffCheck/df_joint_detector.py
+++ src/gh/diffCheck/diffCheck/df_joint_detector.py
@@ -16,6 +16,7 @@ class JointDetector:
     This class is responsible for detecting joints in a brep
     """
     brep: Rhino.Geometry.Brep
+    is_roundwood: bool
 
     def __post_init__(self):
         self._faces = []
@@ -47,7 +48,7 @@ class JointDetector:
 
         return extended_ids
 
-    def find_largest_cylinder(self):
+    def _find_largest_cylinder(self):
         """
             Finds and returns the largest cylinder in the brep
 
@@ -78,8 +79,59 @@ class JointDetector:
 
         return largest_cylinder
 
+def _find_joint_faces(self, bounding_geometry):
+    """
+    Finds the brep faces that are joint faces.
 
-    def run(self, is_cylinder_beam):
+    the structure of the disctionnary is as follows:
+    {
+        face_id: (face, is_inside)
+        ...
+    }
+        face_id is int
+        face is Rhino.Geometry.BrepFace
+        is_inside is bool
+    """
+    faces = {}
+    if self.is_roundwood:
+        for idx, face in enumerate(self.brep.Faces):
+            faces[idx] = (face, face.IsPlanar(1000 * sc.doc.ModelAbsoluteTolerance))
+    else:
+        for idx, face in enumerate(self.brep.Faces):
+            face_centroid = rg.AreaMassProperties.Compute(face).Centroid
+            coord = face.ClosestPoint(face_centroid)
+            projected_centroid = face.PointAt(coord[1], coord[2])
+            faces[idx] = (face, bounding_geometry.IsPointInside(projected_centroid, sc.doc.ModelAbsoluteTolerance, True))
+
+        return faces
+
+def _compute_adjacency_of_faces(faces):
+    """
+    Computes the adjacency list of each face
+
+    the structure of the dictionnary is as follows:
+    {
+        face_id: (face, [adj_face_id_1, adj_face_id_2, ...])
+        ...
+    }
+        face_id is int
+        face is Rhino.Geometry.BrepFace
+        adj_face_id_1, adj_face_id_2, ... are int
+    """
+    adjacency_of_faces = {}
+    for idx, face in faces.items():
+        if not face[1]:
+            continue
+        adjacency_of_faces[idx] = (face[0],
+                                   [adj_face
+                                    for adj_face in face[0].AdjacentFaces()
+                                    if faces[adj_face][1]
+                                        and faces[adj_face][0].IsPlanar(1000 * sc.doc.ModelAbsoluteTolerance)
+                                        and adj_face != idx])
+
+    return adjacency_of_faces
+
+def run(self):
         """
             Run the joint detector. We use a dictionary to store the faces of the cuts based wethear they are cuts or holes.
             - for cuts: If it is a cut we return the face, and the id of the joint the faces belongs to.
@@ -91,8 +143,8 @@ class JointDetector:
         # brep vertices to cloud
         df_cloud = diffCheck.diffcheck_bindings.dfb_geometry.DFPointCloud()
         df_cloud.points = [np.array([vertex.Location.X, vertex.Location.Y, vertex.Location.Z]).reshape(3, 1) for vertex in self.brep.Vertices]
-        if is_cylinder_beam:
-            cylinder = self.find_largest_cylinder()
+        if self.is_roundwood:
+            cylinder = self._find_largest_cylinder()
             bounding_geometry = cylinder
         else:
             bounding_geometry = diffCheck.df_cvt_bindings.cvt_dfOBB_2_rhbrep(df_cloud.get_tight_bounding_box())
@@ -114,44 +166,8 @@ class JointDetector:
         )
         bounding_geometry.Transform(xform)
 
-        # check if face's centers are inside the OBB
-        '''
-        the structure of the disctionnary is as follows:
-        {
-            face_id: (face, is_inside)
-            ...
-        }
-            face_id is int
-            face is Rhino.Geometry.BrepFace
-            is_inside is bool
-        '''
-        faces = {}
-        if is_cylinder_beam:
-            for idx, face in enumerate(self.brep.Faces):
-                faces[idx] = (face, face.IsPlanar(1000 * sc.doc.ModelAbsoluteTolerance))
-        else:
-            for idx, face in enumerate(self.brep.Faces):
-                face_centroid = rg.AreaMassProperties.Compute(face).Centroid
-                coord = face.ClosestPoint(face_centroid)
-                projected_centroid = face.PointAt(coord[1], coord[2])
-                faces[idx] = (face, bounding_geometry.IsPointInside(projected_centroid, sc.doc.ModelAbsoluteTolerance, True))
-
-        # compute the adjacency list of each face
-        adjacency_of_faces = {}
-        '''
-        the structure of the dictionnary is as follows:
-        {
-            face_id: (face, [adj_face_id_1, adj_face_id_2, ...])
-            ...
-        }
-            face_id is int
-            face is Rhino.Geometry.BrepFace
-            adj_face_id_1, adj_face_id_2, ... are int
-        '''
-        for idx, face in faces.items():
-            if not face[1]:
-                continue
-            adjacency_of_faces[idx] = (face[0], [adj_face for adj_face in face[0].AdjacentFaces() if faces[adj_face][1] and faces[adj_face][0].IsPlanar(1000 * sc.doc.ModelAbsoluteTolerance) and adj_face != idx])
+        faces = _find_joint_faces(self, bounding_geometry)
+        adjacency_of_faces = _compute_adjacency_of_faces(self, faces)
         adjacency_of_faces = diffCheck.df_util.merge_shared_indexes(adjacency_of_faces)
         joint_face_ids = [[key] + value[1] for key, value in adjacency_of_faces.items()]
 
