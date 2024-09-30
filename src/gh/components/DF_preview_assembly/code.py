9b41c87afbc28cced1595f4568ee3d20001a8ab3
Andrea Settimi <andreasettimi39@gmail.com>
2024-09-30 21:30:36 +0200

    FIX: no arrow for single beam

--- src/gh/components/DF_preview_assembly/code.py
+++ src/gh/components/DF_preview_assembly/code.py
@@ -96,29 +96,30 @@ class DFPreviewAssembly(component):
             #######################################
             ## DFBeams
             #######################################
-            # beams' obb
-            df_cloud = diffCheck.diffcheck_bindings.dfb_geometry.DFPointCloud()
-            vertices_pt3d_rh : typing.List[rg.Point3d] = [vertex.to_rg_point3d() for vertex in beam.vertices]
-            df_cloud.points = [np.array([vertex.X, vertex.Y, vertex.Z]).reshape(3, 1) for vertex in vertices_pt3d_rh]
-            obb: rg.Brep = diffCheck.df_cvt_bindings.cvt_dfOBB_2_rhbrep(df_cloud.get_tight_bounding_box())
-            # args.Display.DrawBrepWires(obb, System.Drawing.Color.Red)  ## keep for debugging
+            if len(self._dfassembly.beams) > 1:
+                # beams' obb
+                df_cloud = diffCheck.diffcheck_bindings.dfb_geometry.DFPointCloud()
+                vertices_pt3d_rh : typing.List[rg.Point3d] = [vertex.to_rg_point3d() for vertex in beam.vertices]
+                df_cloud.points = [np.array([vertex.X, vertex.Y, vertex.Z]).reshape(3, 1) for vertex in vertices_pt3d_rh]
+                obb: rg.Brep = diffCheck.df_cvt_bindings.cvt_dfOBB_2_rhbrep(df_cloud.get_tight_bounding_box())
+                # args.Display.DrawBrepWires(obb, System.Drawing.Color.Red)  ## keep for debugging
 
-            # axis arrow
-            obb_faces = obb.Faces
-            obb_faces = sorted(obb_faces, key=lambda face: rg.AreaMassProperties.Compute(face).Area)
-            obb_endfaces = obb_faces[:2]
-            beam_axis = rg.Line(obb_endfaces[0].GetBoundingBox(True).Center, obb_endfaces[1].GetBoundingBox(True).Center)
-            extension_length = 0.5 * diffCheck.df_util.get_doc_2_meters_unitf()
-            beam_axis.Extend(extension_length, extension_length)
-            args.Display.DrawArrow(beam_axis, System.Drawing.Color.Magenta)
+                # axis arrow
+                obb_faces = obb.Faces
+                obb_faces = sorted(obb_faces, key=lambda face: rg.AreaMassProperties.Compute(face).Area)
+                obb_endfaces = obb_faces[:2]
+                beam_axis = rg.Line(obb_endfaces[0].GetBoundingBox(True).Center, obb_endfaces[1].GetBoundingBox(True).Center)
+                extension_length = 0.5 * diffCheck.df_util.get_doc_2_meters_unitf()
+                beam_axis.Extend(extension_length, extension_length)
+                args.Display.DrawArrow(beam_axis, System.Drawing.Color.Magenta)
 
-            # beam assembly index
-            anchor_pt: rg.Point3d = beam_axis.From - beam_axis.UnitTangent * 0.5 * extension_length
-            args.Display.Draw2dText(
-                str(beam.index_assembly),
-                System.Drawing.Color.Violet,
-                anchor_pt,
-                True, 18)
+                # beam assembly index
+                anchor_pt: rg.Point3d = beam_axis.From - beam_axis.UnitTangent * 0.5 * extension_length
+                args.Display.Draw2dText(
+                    str(beam.index_assembly),
+                    System.Drawing.Color.Violet,
+                    anchor_pt,
+                    True, 18)
 
             #######################################
             ## DFJoints
@@ -136,7 +137,7 @@ class DFPreviewAssembly(component):
 
                         vector_face_center_2_beam_center = face_center - beam.center
                         vector_face_center_2_beam_center.Unitize()
-                        vector_face_center_2_beam_center *= 0.4 * extension_length
+                        vector_face_center_2_beam_center *= 0.4 * 0.5 * diffCheck.df_util.get_doc_2_meters_unitf()
 
                         ln = rg.Line(face_center, face_center + vector_face_center_2_beam_center)
                         args.Display.DrawDottedLine(ln, clr)
