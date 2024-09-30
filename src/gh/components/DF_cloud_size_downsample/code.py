90fde66120b576b10a6d9487ac20508456259bdd
Andrea Settimi <andreasettimi39@gmail.com>
2024-09-30 17:25:32 +0200

    FIX: canvas drop error

--- src/gh/components/DF_cloud_size_downsample/code.py
+++ src/gh/components/DF_cloud_size_downsample/code.py
@@ -8,7 +8,11 @@ from ghpythonlib.componentbase import executingcomponent as component
 from diffCheck import df_cvt_bindings
 
 class DFCloudSizeDownsample(component):
-    def RunScript(self, i_cloud: Rhino.Geometry.PointCloud, i_size: float) -> Rhino.Geometry.PointCloud:
+    def RunScript(self,
+        i_cloud: Rhino.Geometry.PointCloud,
+        i_size: float) -> Rhino.Geometry.PointCloud:
+        if i_cloud or i_size is None:
+            return None
         df_cloud = df_cvt_bindings.cvt_rhcloud_2_dfcloud(i_cloud)
         df_cloud.downsample_by_size(i_size)
         o_cloud = df_cvt_bindings.cvt_dfcloud_2_rhcloud(df_cloud)
