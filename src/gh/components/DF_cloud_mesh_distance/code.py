464fedc35d0237dfd94abbb8bf41d56500234859
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2024-10-01 00:10:43 +0200

    UPDATE-FIX: assembly joint faces meshes in the same order as pc segments

--- src/gh/components/DF_cloud_mesh_distance/code.py
+++ src/gh/components/DF_cloud_mesh_distance/code.py
@@ -41,7 +41,11 @@ class DFCloudMeshDistance(component):
             rh_mesh_target_list = [joint.to_mesh(i_analysis_resolution) for joint in i_assembly._all_joints]
         elif len(i_assembly.all_joint_faces) == len(i_cloud_list):
             ghenv.Component.Message = "Per Joint Face"  # noqa: F821
-            rh_mesh_target_list = [joint_face.to_mesh() for joint_face in i_assembly.all_joint_faces]
+            rh_mesh_target_list = []
+            for joint in i_assembly.all_joints:
+                for face in joint.faces:
+                    face = face.to_mesh()
+                    rh_mesh_target_list.append(face)
         else:
             ghenv.Component.AddRuntimeMessage(RML.Warning, "The input number of objects to compare matches neither the number of beams nor the number of joints")  # noqa: F821
             return None, None, None, None, None, None
