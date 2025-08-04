3eebaa567eb9c90caa23cbdb3f3f405930c1fadd
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2025-08-04 23:39:40 +0200

    feat: improve cropping component following @eleniv3d 's comments

--- src/gh/components/DF_crop_cloud/code.py
+++ src/gh/components/DF_crop_cloud/code.py
@@ -4,7 +4,6 @@ from diffCheck import df_cvt_bindings as df_cvt
 import numpy as np
 
 import Rhino
-from Grasshopper.Kernel import GH_RuntimeMessageLevel as RML
 
 from ghpythonlib.componentbase import executingcomponent as component
 
@@ -16,36 +15,28 @@ class DFCloudCrop(component):
 
     def RunScript(self,
             i_cloud: Rhino.Geometry.PointCloud,
-            i_box: Rhino.Geometry.Brep,
-            i_brep: Rhino.Geometry.Brep):
-        if i_cloud is None:
-            ghenv.Component.AddRuntimeMessage(RML.Warning,"No point cloud provided. Please connect a point cloud to the input.")# noqa: F821
-            return None
+            i_boundary: Rhino.Geometry.Brep):
 
-        if i_box is not None:
-            bbox = i_box.GetBoundingBox(True)
-            bb_min_as_array = np.asarray([bbox.Min.X, bbox.Min.Y, bbox.Min.Z])
-            bb_max_as_array = np.asarray([bbox.Max.X, bbox.Max.Y, bbox.Max.Z])
+        if i_boundary.IsBox():
+            vertices = i_boundary.Vertices
+            bb_as_array = [np.asarray([vertice.Location.X, vertice.Location.Y, vertice.Location.Z]) for vertice in vertices]
             df_cloud = df_cvt.cvt_rhcloud_2_dfcloud(i_cloud)
             df_cloud_copy = df_cloud.duplicate()
-            df_cloud.crop(bb_min_as_array, bb_max_as_array)
+            df_cloud.crop(bb_as_array)
             df_cloud_copy.subtract_points(df_cloud, TOL)
             o_pts_out = df_cvt.cvt_dfcloud_2_rhcloud(df_cloud_copy)
             o_pts_in = df_cvt.cvt_dfcloud_2_rhcloud(df_cloud)
 
-        elif i_brep is not None:
+        else:
             pts_in =  []
             pts_out = []
             for pc_item in i_cloud:
                 point = Rhino.Geometry.Point3d(pc_item.X, pc_item.Y, pc_item.Z)
-                if i_brep.IsPointInside(point, TOL, True):
+                if i_boundary.IsPointInside(point, TOL, True):
                     pts_in.append(point)
                 else:
                     pts_out.append(point)
             o_pts_in = Rhino.Geometry.PointCloud(pts_in)
             o_pts_out = Rhino.Geometry.PointCloud(pts_out)
-
-        else:
-            ghenv.Component.AddRuntimeMessage(RML.Warning, "Please provide a box to crop the point cloud with") # noqa: F821
 
         return [o_pts_in, o_pts_out]
