5745db8d2ecedf26f0172bd667669674254f6a44
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2025-09-02 23:48:20 +0200

    feat: update DFMainPCAxes to save the new poses only when user triggers it

--- src/gh/components/DF_main_pc_axes/code.py
+++ src/gh/components/DF_main_pc_axes/code.py
@@ -1,6 +1,5 @@
 #! python3
 
-from diffCheck import diffcheck_bindings
 from diffCheck import df_cvt_bindings
 from diffCheck import df_poses
 
@@ -14,6 +13,8 @@ import System
 class DFMainPCAxes(component):
     def RunScript(self,
             i_clouds: System.Collections.Generic.List[Rhino.Geometry.PointCloud],
+            i_assembly,
+            i_save: bool,
             i_reset: bool):
 
         planes = []
@@ -22,7 +23,6 @@ class DFMainPCAxes(component):
             all_poses_in_time.reset()
             return None, None
 
-        previous_poses = all_poses_in_time.get_last_poses()
         all_poses_this_time = []
         for i, cloud in enumerate(i_clouds):
             df_cloud = df_cvt_bindings.cvt_rhcloud_2_dfcloud(cloud)
@@ -34,23 +34,13 @@ class DFMainPCAxes(component):
             df_points = df_cloud.get_axis_aligned_bounding_box()
             df_point = (df_points[0] + df_points[1]) / 2
             rh_point = Rhino.Geometry.Point3d(df_point[0], df_point[1], df_point[2])
-            vectors = []
-            # Get the main axes of the point cloud
-            previous_pose = previous_poses[i] if previous_poses else None
-            if previous_pose:
-                rh_previous_xDirection = Rhino.Geometry.Vector3d(previous_pose.xDirection[0], previous_pose.xDirection[1], previous_pose.xDirection[2])
-                rh_previous_yDirection = Rhino.Geometry.Vector3d(previous_pose.yDirection[0], previous_pose.yDirection[1], previous_pose.yDirection[2])
-                n_faces = all_poses_in_time.poses_per_element_dictionary[f"element_{i}"].n_faces
-            else:
-                rh_previous_xDirection = None
-                rh_previous_yDirection = None
-                n_faces = len(diffcheck_bindings.dfb_segmentation.DFSegmentation.segment_by_normal(df_cloud, 12, int(len(df_cloud.points)/20), True, int(len(df_cloud.points)/200), 1))
 
-            axes = df_cloud.get_principal_axes(n_faces)
+            axes = df_cloud.get_principal_axes(3)
+            vectors = []
             for axe in axes:
                 vectors.append(Rhino.Geometry.Vector3d(axe[0], axe[1], axe[2]))
 
-            new_xDirection, new_yDirection = df_poses.select_vectors(vectors, rh_previous_xDirection, rh_previous_yDirection)
+            new_xDirection, new_yDirection = df_poses.select_vectors(vectors, i_assembly.beams[i].plane.XAxis, i_assembly.beams[i].plane.YAxis)
 
             pose = df_poses.DFPose(
                 origin = [rh_point.X, rh_point.Y, rh_point.Z],
@@ -60,6 +50,7 @@ class DFMainPCAxes(component):
             plane = Rhino.Geometry.Plane(origin = rh_point, xDirection=new_xDirection, yDirection=new_yDirection)
             planes.append(plane)
 
-        all_poses_in_time.add_step(all_poses_this_time)
+        if i_save:
+            all_poses_in_time.add_step(all_poses_this_time)
 
         return [planes, all_poses_in_time]
