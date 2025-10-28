28d6bb2637599fcbfc32b5cc0eafb87e13b98ebb
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2025-10-28 16:03:55 +0100

    feat-wip: use assembly instead of explicit list of planes

--- src/gh/components/DF_pose_comparison/code.py
+++ src/gh/components/DF_pose_comparison/code.py
@@ -5,18 +5,21 @@ from ghpythonlib.componentbase import executingcomponent as component
 
 import System
 
+import diffCheck.df_geometries
 import numpy
 
 
 class DFPoseComparison(component):
     def RunScript(self,
-            i_CAD_poses: System.Collections.Generic.List[Rhino.Geometry.Plane],
+            i_assembly: diffCheck.df_geometries.DFAssembly,
             i_measured_poses: System.Collections.Generic.List[Rhino.Geometry.Plane]):
 
-        if len(i_CAD_poses) != len(i_measured_poses):
+        if len(i_assembly.beams) != len(i_measured_poses):
             ghenv.Component.Message = "evaluation during assembly" # noqa: F821
         else:
             ghenv.Component.Message = "evaluation of completed assembly" # noqa: F821
+
+        CAD_poses = [beam.plane for beam in i_assembly.beams]
 
         o_distances = []
         o_angles = []
@@ -24,13 +27,13 @@ class DFPoseComparison(component):
         # Compare the origins
         #   measure the distance between the origins of the CAD pose and the measured pose and output this in the component
         for i in range(len(i_measured_poses)):
-            cad_origin = i_CAD_poses[i].Origin
+            cad_origin = CAD_poses[i].Origin
             measured_origin = i_measured_poses[i].Origin
             distance = cad_origin.DistanceTo(measured_origin)
             o_distances.append(distance)
 
             # Compare the orientations using the formula: $$ \theta = \arccos\left(\frac{\text{trace}(R_{\text{pred}}^T R_{\text{meas}}) - 1}{2}\right) $$
-            transform_o_to_cad = Rhino.Geometry.Transform.PlaneToPlane(Rhino.Geometry.Plane.WorldXY, i_CAD_poses[i])
+            transform_o_to_cad = Rhino.Geometry.Transform.PlaneToPlane(Rhino.Geometry.Plane.WorldXY, CAD_poses[i])
             transform_o_to_measured = Rhino.Geometry.Transform.PlaneToPlane(Rhino.Geometry.Plane.WorldXY, i_measured_poses[i])
             np_transform_o_to_cad = numpy.array(transform_o_to_cad.ToDoubleArray(rowDominant=True)).reshape((4, 4))
             np_transform_o_to_measured = numpy.array(transform_o_to_measured.ToDoubleArray(rowDominant=True)).reshape((4, 4))
@@ -42,7 +45,7 @@ class DFPoseComparison(component):
             o_angles.append(theta)
 
             # Compute the transformation matrix between the CAD pose and the measured pose
-            transform_cad_to_measured = Rhino.Geometry.Transform.PlaneToPlane(i_CAD_poses[i], i_measured_poses[i])
+            transform_cad_to_measured = Rhino.Geometry.Transform.PlaneToPlane(CAD_poses[i], i_measured_poses[i])
             o_transforms_cad_to_measured.append(transform_cad_to_measured)
 
         return [o_distances, o_angles, o_transforms_cad_to_measured]
