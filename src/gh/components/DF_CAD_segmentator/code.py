3811b56c6cbe4f7f4410b2e32ff93d174a29ed89
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2026-02-11 17:28:03 +0100

    feat: add ICP registration in CAD segmentation component and expose basic parameters

--- src/gh/components/DF_CAD_segmentator/code.py
+++ src/gh/components/DF_CAD_segmentator/code.py
@@ -9,8 +9,7 @@ import ghpythonlib.treehelpers as th
 
 
 from diffCheck.diffcheck_bindings import dfb_segmentation
-from diffCheck.diffcheck_bindings import dfb_geometry, dfb_registrations
-# from diffCheck.diffCheck_bindings import dfb_registrations
+from diffCheck.diffcheck_bindings import dfb_geometry, dfb_registrations, dfb_transformation
 
 from diffCheck import df_cvt_bindings
 
@@ -22,7 +21,10 @@ class DFCADSegmentator(component):
             i_assembly,
             i_angle_threshold: float,
             i_association_threshold: float,
-            i_angle_association_threshold: float):
+            i_angle_association_threshold: float,
+            i_maximum_face_segment_distance: float,
+            i_radius_normal_estimation: float,
+            i_max_correspondence_distance_icp: float,):
 
         if i_clouds is None or i_assembly is None:
             self.AddRuntimeMessage(RML.Warning, "Please provide a cloud and an assembly to segment.")
@@ -33,12 +35,17 @@ class DFCADSegmentator(component):
             i_association_threshold = 0.1
         if i_angle_association_threshold is None:
             i_angle_association_threshold = 0.5
+        if i_radius_normal_estimation is None:
+            i_radius_normal_estimation = 0.01
         o_face_clusters = []
         df_clusters_temp = []
         df_clusters = []
         # we make a deepcopy of the input clouds
         df_clouds_copy = [df_cvt_bindings.cvt_rhcloud_2_dfcloud(cloud.Duplicate()) for cloud in i_clouds]
         df_clouds = [df_cvt_bindings.cvt_rhcloud_2_dfcloud(cloud.Duplicate()) for cloud in i_clouds]
+        df_merged_cloud = dfb_geometry.DFPointCloud()
+        for pc in df_clouds:
+            df_merged_cloud.add_points(pc)
 
         df_beams = i_assembly.beams
         df_asssociated_cluster_faces_per_beam = []
@@ -50,32 +57,31 @@ class DFCADSegmentator(component):
             # different association depending on the type of beam
             df_asssociated_cluster_faces = dfb_segmentation.DFSegmentation.associate_clusters(
                 is_roundwood=df_b.is_roundwood,
-                discriminate_points=False,
+                discriminate_points=True,
                 reference_mesh=df_b_mesh_faces,
                 unassociated_clusters=df_clouds_copy,
                 angle_threshold=i_angle_threshold,
-                association_threshold=i_association_threshold,
-                angle_association_threshold=i_angle_association_threshold
+                association_threshold=i_association_threshold ,
+                angle_association_threshold=i_angle_association_threshold,
+                maximum_face_segment_distance=i_maximum_face_segment_distance
             )
             df_asssociated_cluster_faces_per_beam.append(df_asssociated_cluster_faces)
 
         for i, df_b in enumerate(df_beams):
-            # o_face_clusters.append([])
             rh_b_mesh_faces = [df_b_f.to_mesh() for df_b_f in df_b.side_faces]
             df_b_mesh_faces = [df_cvt_bindings.cvt_rhmesh_2_dfmesh(rh_b_mesh_face) for rh_b_mesh_face in rh_b_mesh_faces]
 
             dfb_segmentation.DFSegmentation.clean_unassociated_clusters(
                 is_roundwood=df_b.is_roundwood,
-                discriminate_points=False,
+                discriminate_points=True,
                 unassociated_clusters=df_clouds_copy,
                 associated_clusters=[df_asssociated_cluster_faces_per_beam[i]],
                 reference_mesh=[df_b_mesh_faces],
                 angle_threshold=i_angle_threshold,
-                association_threshold=i_association_threshold,
-                angle_association_threshold=i_angle_association_threshold
+                association_threshold=i_association_threshold ,
+                angle_association_threshold=i_angle_association_threshold,
+                maximum_face_segment_distance=i_maximum_face_segment_distance
             )
-
-            # o_face_clusters[-1] = [df_cvt_bindings.cvt_dfcloud_2_rhcloud(cluster) for cluster in df_asssociated_cluster_faces_per_beam[i]]
 
             df_asssociated_cluster = dfb_geometry.DFPointCloud()
             for df_associated_face in df_asssociated_cluster_faces_per_beam[i]:
@@ -86,23 +92,36 @@ class DFCADSegmentator(component):
         # Now with the df_clusters, we sample a point cloud on the beam of the assembly, and perform an ICP to align the beam mesh to the point cloud.
         # Then we re-compute the association on the scan with thigher thresholds to have a better segmentation.
         o_transforms = []
+        rh_meshes = []
         for i, df_b in enumerate(df_beams):
             rh_b_mesh_faces = [df_b_f.to_mesh() for df_b_f in df_b.side_faces]
+            for df_j_face in df_b.joint_faces:
+                rh_b_mesh_faces.append(df_j_face.to_mesh())
             rh_mesh = Rhino.Geometry.Mesh()
             for rh_b_mesh_face in rh_b_mesh_faces:
                 rh_mesh.Append(rh_b_mesh_face)
-            df_b_mesh = df_cvt_bindings.cvt_rhmesh_2_dfmesh(rh_mesh)
-            df_sampled_cloud = df_b_mesh.sample_points_uniformly(10000)
-            df_sampled_cloud.estimate_normals(use_cilantro_evaluator=True,
-            knn = 10,
-            )
-            print(df_clusters_temp[i].get_num_points())
-            transform = dfb_registrations.DFRefinedRegistration.O3DGeneralizedICP(
-                source=df_sampled_cloud,
-                target=df_clusters_temp[i],
-                max_correspondence_distance= 0.03
 
+            df_b_mesh = df_cvt_bindings.cvt_rhmesh_2_dfmesh(rh_mesh)
+            df_sampled_cloud = df_b_mesh.sample_points_uniformly(1000)
+            df_sampled_cloud.estimate_normals(use_cilantro_evaluator=False,
+            search_radius = i_radius_normal_estimation,
             )
+            cluster_without_normals = df_clusters_temp[i]
+            cluster_without_normals.estimate_normals(use_cilantro_evaluator=False,
+            search_radius = i_radius_normal_estimation,
+            )
+            df_merged_cloud.remove_statistical_outliers(100, 1.5)
+            self.AddRuntimeMessage(RML.Warning, f"size of pc: {cluster_without_normals.get_num_points()}")
+            if cluster_without_normals.get_num_points() != 0:
+                transform = dfb_registrations.DFRefinedRegistration.O3DICP(
+                    source=df_sampled_cloud,
+                    target=df_merged_cloud,
+                    max_correspondence_distance= i_max_correspondence_distance_icp,
+                    max_iteration = 1000
+
+                )
+            else:
+                transform = dfb_transformation.DFTransformation()
             df_xform = transform.transformation_matrix
             rh_xform = Rhino.Geometry.Transform()
             for i in range(4):
@@ -113,8 +132,12 @@ class DFCADSegmentator(component):
         df_new_asssociated_cluster_faces_per_beam = []
         for i, df_b in enumerate(df_beams):
             rh_b_mesh_faces = [df_b_f.to_mesh() for df_b_f in df_b.side_faces]
-            for rh_mesh in rh_b_mesh_faces:
-                rh_mesh.Transform(o_transforms[i])
+            rh_test_mesh = Rhino.Geometry.Mesh()
+            for j in range(len(rh_b_mesh_faces)):
+                sucess = rh_b_mesh_faces[j].Transform(o_transforms[i])
+                if sucess:
+                    rh_test_mesh.Append(rh_b_mesh_faces[j])
+            rh_meshes.append(rh_test_mesh)
             df_b_mesh_faces = [df_cvt_bindings.cvt_rhmesh_2_dfmesh(rh_b_mesh_face) for rh_b_mesh_face in rh_b_mesh_faces]
 
             # different association depending on the type of beam
@@ -125,15 +148,16 @@ class DFCADSegmentator(component):
                 unassociated_clusters=df_clouds,
                 angle_threshold=i_angle_threshold,
                 association_threshold=i_association_threshold,
-                angle_association_threshold=i_angle_association_threshold
+                angle_association_threshold=i_angle_association_threshold,
+                maximum_face_segment_distance=i_maximum_face_segment_distance
             )
             df_new_asssociated_cluster_faces_per_beam.append(df_new_asssociated_cluster_faces)
 
         for i, df_b in enumerate(df_beams):
             o_face_clusters.append([])
             rh_b_mesh_faces = [df_b_f.to_mesh() for df_b_f in df_b.side_faces]
-            for rh_mesh in rh_b_mesh_faces:
-                rh_mesh.Transform(o_transforms[i])
+            for j in range(len(rh_b_mesh_faces)):
+                rh_b_mesh_faces[j].Transform(o_transforms[i])
             df_b_mesh_faces = [df_cvt_bindings.cvt_rhmesh_2_dfmesh(rh_b_mesh_face) for rh_b_mesh_face in rh_b_mesh_faces]
 
             dfb_segmentation.DFSegmentation.clean_unassociated_clusters(
@@ -144,7 +168,8 @@ class DFCADSegmentator(component):
                 reference_mesh=[df_b_mesh_faces],
                 angle_threshold=i_angle_threshold,
                 association_threshold=i_association_threshold,
-                angle_association_threshold=i_angle_association_threshold
+                angle_association_threshold=i_angle_association_threshold,
+                maximum_face_segment_distance=i_maximum_face_segment_distance
             )
 
             o_face_clusters[-1] = [df_cvt_bindings.cvt_dfcloud_2_rhcloud(cluster) for cluster in df_new_asssociated_cluster_faces_per_beam[i]]
@@ -155,6 +180,7 @@ class DFCADSegmentator(component):
 
             df_clusters.append(df_asssociated_cluster)
 
+        o_beam_intermediary_clouds = [df_cvt_bindings.cvt_dfcloud_2_rhcloud(cluster) for cluster in df_clusters_temp]
         o_beam_clouds = [df_cvt_bindings.cvt_dfcloud_2_rhcloud(cluster) for cluster in df_clusters]
 
         for i, o_beam_cloud in enumerate(o_beam_clouds):
@@ -164,4 +190,4 @@ class DFCADSegmentator(component):
 
         o_face_clouds = th.list_to_tree(o_face_clusters)
 
-        return [o_beam_clouds, o_face_clouds, o_transforms]
+        return [o_beam_clouds, o_face_clouds, o_transforms, o_beam_intermediary_clouds, rh_meshes]
