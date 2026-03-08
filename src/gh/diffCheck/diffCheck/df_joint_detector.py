2325233c231329472a666e9c2dbc8c359501e2e5
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2026-03-08 18:35:32 +0100

    fix: slight refactor of .run method in JointDetector

--- src/gh/diffCheck/diffCheck/df_joint_detector.py
+++ src/gh/diffCheck/diffCheck/df_joint_detector.py
@@ -7,8 +7,6 @@ from dataclasses import dataclass
 import diffCheck.df_util
 import diffCheck.df_transformations
 
-import numpy as np
-
 
 @dataclass
 class JointDetector:
@@ -140,24 +138,21 @@ class JointDetector:
 
                 :return: a list of faces from joins and faces
             """
-
-            # brep vertices to cloud
-            df_cloud = diffCheck.diffcheck_bindings.dfb_geometry.DFPointCloud()
-            df_cloud.points = [np.array([vertex.Location.X, vertex.Location.Y, vertex.Location.Z]).reshape(3, 1) for vertex in self.brep.Vertices]
             if self.is_roundwood:
                 bounding_geometry = self._find_largest_cylinder()
             else:
-                bounding_geometry = diffCheck.df_cvt_bindings.cvt_dfOBB_2_rhbrep(df_cloud.get_tight_bounding_box())
+                bounding_geometry = diffCheck.df_util.compute_oriented_bounding_box(self.brep)
 
             # scale the bounding geometry in the longest edge direction by 1.5 from center on both directions
-            rh_Bounding_geometry_center = bounding_geometry.GetBoundingBox(True).Center
+            rh_Bounding_geometry_center = Rhino.Geometry.AreaMassProperties.Compute(bounding_geometry).Centroid
             edges = bounding_geometry.Edges
             edge_lengths = [edge.GetLength() for edge in edges]
             longest_edge = edges[edge_lengths.index(max(edge_lengths))]
 
             rh_Bounding_geometry_zaxis = rg.Vector3d(longest_edge.PointAt(1) - longest_edge.PointAt(0))
+            self.boudning_geometry_z_axis = rh_Bounding_geometry_zaxis
             rh_Bounding_geometry_plane = rg.Plane(rh_Bounding_geometry_center, rh_Bounding_geometry_zaxis)
-            scale_factor = 0.1
+            scale_factor = 0.15
             xform = rg.Transform.Scale(
                 rh_Bounding_geometry_plane,
                 1 - scale_factor,
