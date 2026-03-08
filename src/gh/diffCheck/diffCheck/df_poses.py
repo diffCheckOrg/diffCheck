e87984ac57f8f91cab4eefe46a1c90c9bfd3c08c
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2026-03-08 22:22:36 +0100

    feat: align detected pose vector to DFBeam's (no vector reversing)

--- src/gh/diffCheck/diffCheck/df_poses.py
+++ src/gh/diffCheck/diffCheck/df_poses.py
@@ -132,25 +132,23 @@ def select_vectors(vectors, previous_xDirection, previous_yDirection):
     if previous_xDirection is not None and previous_yDirection is not None:
         sorted_vectors_by_alignment = sorted(vectors, key=lambda v: abs(compute_dot_product(v, previous_xDirection)), reverse=True)
         new_xDirection = sorted_vectors_by_alignment[0]
+
     else:
         new_xDirection = vectors[0]
-
-    condidates_for_yDirection = []
-    for v in vectors:
-        if compute_dot_product(v, new_xDirection) ** 2 < 0.5:
-            condidates_for_yDirection.append(v)
-
-    if not condidates_for_yDirection:
-        return new_xDirection, None
-
+    new_xDirection.Unitize()
     if previous_xDirection is not None and previous_yDirection is not None:
-        sorted_vectors_by_perpendicularity = sorted(condidates_for_yDirection, key=lambda v: abs(compute_dot_product(v, previous_yDirection)), reverse=True)
-        new_xDirection = sorted_vectors_by_alignment[0]
+        sorted_vectors_by_perpendicularity = sorted(vectors, key=lambda v: abs(compute_dot_product(v, previous_xDirection)))
         new_yDirection = sorted_vectors_by_perpendicularity[0] - compute_dot_product(sorted_vectors_by_perpendicularity[0], new_xDirection) * new_xDirection
+        if compute_dot_product(new_xDirection, previous_xDirection) < 0:
+            new_xDirection = -sorted_vectors_by_alignment[0]
+        if compute_dot_product(new_yDirection, previous_yDirection) < 0:
+            new_yDirection = -new_yDirection
         new_yDirection.Unitize()
     else:
 
-        sorted_vectors = sorted(vectors[1:], key=lambda v: compute_dot_product(v, new_xDirection)**2)
+        sorted_vectors = sorted(vectors[1:], key=lambda v: abs(compute_dot_product(v, new_xDirection)))
         new_yDirection = sorted_vectors[0] - compute_dot_product(sorted_vectors[0], new_xDirection) * new_xDirection
+        if previous_yDirection is not None and compute_dot_product(new_yDirection, previous_yDirection) < 0:
+            new_yDirection = -new_yDirection
         new_yDirection.Unitize()
     return new_xDirection, new_yDirection
