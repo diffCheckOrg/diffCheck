e8c26700d1d145ad6f31962af4c31b723b3adafa
DamienGilliard <127743632+DamienGilliard@users.noreply.github.com>
2026-03-22 12:19:04 +0100

    feat: add input parameter i_poses_from_icp to accept poses detected ad CAD segmentation step

--- src/gh/components/DF_pose_estimation/code.py
+++ src/gh/components/DF_pose_estimation/code.py
@@ -17,6 +17,7 @@ class DFPoseEstimation(component):
     def RunScript(self,
             i_face_clouds: Grasshopper.DataTree[Rhino.Geometry.PointCloud],
             i_assembly,
+            i_poses_from_icp: list,
             i_reset: bool,
             i_save: bool):
 
@@ -35,40 +36,47 @@ class DFPoseEstimation(component):
         all_poses_this_time = []
         for i, face_clouds in enumerate(clusters_per_beam):
             try:
-                df_cloud = dfb_geometry.DFPointCloud()
+                if len(i_poses_from_icp) > i and i_poses_from_icp[i] is not None:
+                    # if there is a pose from ICP for this beam, use it directly without processing the cloud
+                    planes.append(i_poses_from_icp[i])
+                    all_poses_this_time.append(df_poses.DFPose.from_rh_plane(i_poses_from_icp[i]))
+                    continue
 
-                rh_face_normals = []
-                for face_cloud in face_clouds:
-                    df_face_cloud = df_cvt_bindings.cvt_rhcloud_2_dfcloud(face_cloud)
-                    df_cloud.add_points(df_face_cloud)
-                    plane_normal = df_face_cloud.fit_plane_ransac()
-                    if all(plane_normal) == 0:
-                        ghenv.Component.AddRuntimeMessage(RML.Warning, f"There was a missing face in the cloud of beam {i}: the face was skipped in the pose estimation of that beam")  # noqa: F821
-                        continue
-                    rh_face_normals.append(Rhino.Geometry.Vector3d(plane_normal[0], plane_normal[1], plane_normal[2]))
+                else:
+                    df_cloud = dfb_geometry.DFPointCloud()
 
-                df_bb_points = df_cloud.get_tight_bounding_box()
-                df_bb_centroid = sum(df_bb_points)/len(df_bb_points)
-                rh_tentative_bb_centroid = Rhino.Geometry.Point3d(df_bb_centroid[0], df_bb_centroid[1], df_bb_centroid[2])
+                    rh_face_normals = []
+                    for face_cloud in face_clouds:
+                        df_face_cloud = df_cvt_bindings.cvt_rhcloud_2_dfcloud(face_cloud)
+                        df_cloud.add_points(df_face_cloud)
+                        plane_normal = df_face_cloud.fit_plane_ransac()
+                        if all(plane_normal) == 0:
+                            ghenv.Component.AddRuntimeMessage(RML.Warning, f"There was a missing face in the cloud of beam {i}: the face was skipped in the pose estimation of that beam")  # noqa: F821
+                            continue
+                        rh_face_normals.append(Rhino.Geometry.Vector3d(plane_normal[0], plane_normal[1], plane_normal[2]))
 
-                new_xDirection, new_yDirection = df_poses.select_vectors(rh_face_normals, i_assembly.beams[i].plane.XAxis, i_assembly.beams[i].plane.YAxis)
-                rh_tentative_plane = Rhino.Geometry.Plane(rh_tentative_bb_centroid, new_xDirection, new_yDirection)
+                    df_bb_points = df_cloud.get_tight_bounding_box()
+                    df_bb_centroid = sum(df_bb_points)/len(df_bb_points)
+                    rh_tentative_bb_centroid = Rhino.Geometry.Point3d(df_bb_centroid[0], df_bb_centroid[1], df_bb_centroid[2])
 
-                rh_beam_cloud = Rhino.Geometry.PointCloud()
-                for face_cloud in face_clouds:
-                    rh_beam_cloud.Merge(face_cloud)
+                    new_xDirection, new_yDirection = df_poses.select_vectors(rh_face_normals, i_assembly.beams[i].plane.XAxis, i_assembly.beams[i].plane.YAxis)
+                    rh_tentative_plane = Rhino.Geometry.Plane(rh_tentative_bb_centroid, new_xDirection, new_yDirection)
 
-                rh_bbox = rh_beam_cloud.GetBoundingBox(rh_tentative_plane)
-                rh_bbox.Transform(Rhino.Geometry.Transform.PlaneToPlane(Rhino.Geometry.Plane.WorldXY, rh_tentative_plane))
-                rh_bb_centroid = rh_bbox.Center
+                    rh_beam_cloud = Rhino.Geometry.PointCloud()
+                    for face_cloud in face_clouds:
+                        rh_beam_cloud.Merge(face_cloud)
 
-                pose = df_poses.DFPose(
-                    origin = [rh_bb_centroid.X, rh_bb_centroid.Y, rh_bb_centroid.Z],
-                    xDirection = [new_xDirection.X, new_xDirection.Y, new_xDirection.Z],
-                    yDirection = [new_yDirection.X, new_yDirection.Y, new_yDirection.Z])
-                all_poses_this_time.append(pose)
-                plane = Rhino.Geometry.Plane(origin = rh_bb_centroid, xDirection=new_xDirection, yDirection=new_yDirection)
-                planes.append(plane)
+                    rh_bbox = rh_beam_cloud.GetBoundingBox(rh_tentative_plane)
+                    rh_bbox.Transform(Rhino.Geometry.Transform.PlaneToPlane(Rhino.Geometry.Plane.WorldXY, rh_tentative_plane))
+                    rh_bb_centroid = rh_bbox.Center
+
+                    pose = df_poses.DFPose(
+                        origin = [rh_bb_centroid.X, rh_bb_centroid.Y, rh_bb_centroid.Z],
+                        xDirection = [new_xDirection.X, new_xDirection.Y, new_xDirection.Z],
+                        yDirection = [new_yDirection.X, new_yDirection.Y, new_yDirection.Z])
+                    all_poses_this_time.append(pose)
+                    plane = Rhino.Geometry.Plane(origin = rh_bb_centroid, xDirection=new_xDirection, yDirection=new_yDirection)
+                    planes.append(plane)
 
             except Exception as e:
                 # Any unexpected error on this cloud, skip it and keep going
